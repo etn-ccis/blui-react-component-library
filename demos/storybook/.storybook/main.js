531498bca0c9e9fee3aadaf55c8f201ed0b51fe9
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-03-06 10:32:18 -0500

    This config does nothing

--- demos/storybook/.storybook/main.js
+++ demos/storybook/.storybook/main.js
@@ -7,17 +7,6 @@ module.exports = {
         '@storybook/addon-knobs',
         '@storybook/addon-notes',
         '@storybook/addon-viewport',
-        {
-            name: '@storybook/addon-storysource',
-            options: {
-                rule: {
-                    // test: [/\.stories\.jsx?$/], This is default
-                    include: [path.resolve(__dirname, '../stories')], // You can specify directories
-                },
-                loaderOptions: {
-                    prettierConfig: '@pxblue/prettier-config',
-                },
-            },
-        },
+        '@storybook/addon-storysource',
     ],
 };
