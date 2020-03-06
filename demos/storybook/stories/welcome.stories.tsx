9c8b6956cfbfe469003af4b570a75594a1718a1b
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-03-06 14:34:34 -0500

    Update sentence

--- demos/storybook/stories/welcome.stories.tsx
+++ demos/storybook/stories/welcome.stories.tsx
@@ -86,7 +86,7 @@ stories.add('PX Blue React Components', () => {
                 </Typography>
                 <Typography variant={'h4'}>React Component Library</Typography>
                 <Typography variant={'subtitle1'} className={classes.description}>
-                    Learn about our PX Blue components in the API section or interact with them in the Playground.
+                   Learn about our PX Blue components through our documentation or directly interact with them.
                 </Typography>
 
                 <div className={classes.buttons}>
