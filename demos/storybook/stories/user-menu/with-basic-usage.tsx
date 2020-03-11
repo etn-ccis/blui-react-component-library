2b6dffd80ab11b0ebfe4514dd372eb967f1c90e4
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-03-11 09:44:47 -0400

    Update icon, reorder items, reduce import lines by 1

--- demos/storybook/stories/user-menu/with-basic-usage.tsx
+++ demos/storybook/stories/user-menu/with-basic-usage.tsx
@@ -1,6 +1,5 @@
 import { Avatar } from '@material-ui/core';
-import { Email, Settings } from '@material-ui/icons';
-import SendIcon from '@material-ui/icons/Send';
+import { Email, Settings, ExitToApp } from '@material-ui/icons';
 import { UserMenu, UserMenuGroup, UserMenuItem } from '@pxblue/react-components';
 import { action } from '@storybook/addon-actions';
 import { text } from '@storybook/addon-knobs';
@@ -10,21 +9,21 @@ import React from 'react';
 const menuItems: UserMenuItem[] = [
     {
         itemID: '1',
-        title: 'Log Out',
-        icon: <SendIcon />,
-        onClick: action('Log Out Selected'),
+        title: 'Account',
+        icon: <Settings />,
+        onClick: action('Account Selected'),
     },
     {
         itemID: '2',
-        title: 'Account Settings',
-        icon: <Settings />,
-        onClick: action('Account Settings Selected'),
-    },
-    {
-        itemID: '3',
         title: 'Contact Us',
         icon: <Email />,
         onClick: action('Contact Us Selected'),
+    },
+    {
+        itemID: '3',
+        title: 'Log Out',
+        icon: <ExitToApp />,
+        onClick: action('Log Out Selected'),
     },
 ];
 
