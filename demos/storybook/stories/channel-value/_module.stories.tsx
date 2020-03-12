2dadb81139f6243eefb62e75d20dede8e12d2138
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-03-12 13:39:39 -0400

    Rename font size story, adjust knobs

--- demos/storybook/stories/channel-value/_module.stories.tsx
+++ demos/storybook/stories/channel-value/_module.stories.tsx
@@ -1,6 +1,6 @@
 import { ChannelValue } from '@pxblue/react-components';
 import { COMPONENT_SECTION_NAME } from '../../src/constants';
-import {getReadMe, storyParams, storyWrapper} from '../../src/utils';
+import { getReadMe, storyParams, storyWrapper } from '../../src/utils';
 
 const channelValueModule = {
     title: `${COMPONENT_SECTION_NAME}/Channel Value`,
@@ -11,11 +11,10 @@ const channelValueModule = {
 
 /* Display order goes here */
 export { getReadMeStory } from '../../src/utils';
-export * from './with-value';
+export * from './with-basic-usage';
 export * from './with-units';
 export * from './with-icon';
-export * from './with-extraLarge-font-size';
-export * from './with-all-props';
+export * from './with-fontSize';
+export * from './with-full-config';
 
 export default channelValueModule;
-
