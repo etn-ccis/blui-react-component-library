02de74df529e88ff8ac4930e199183a1dc87e93e
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-03-12 11:45:35 -0400

    Create hero basic usage and icon color story

--- demos/storybook/stories/hero/_module.stories.tsx
+++ demos/storybook/stories/hero/_module.stories.tsx
@@ -1,9 +1,11 @@
 import { Hero } from '@pxblue/react-components';
-import React from "react";
+import React from 'react';
 import { COMPONENT_SECTION_NAME } from '../../src/constants';
-import {getReadMe, storyParams, storyWrapper} from '../../src/utils';
+import { getReadMe, storyParams, storyWrapper } from '../../src/utils';
 
-const centerHero = (storyFn: any): JSX.Element => <div style={{ alignItems: 'center', display: 'flex', height: '100%' }}>{storyFn()}</div>;
+const centerHero = (storyFn: any): JSX.Element => (
+    <div style={{ alignItems: 'center', display: 'flex', height: '100%' }}>{storyFn()}</div>
+);
 
 const heroModule = {
     title: `${COMPONENT_SECTION_NAME}/Hero`,
@@ -14,8 +16,10 @@ const heroModule = {
 
 /* Display order goes here */
 export { getReadMeStory } from '../../src/utils';
-export * from './with-basic-props';
+export * from './with-basic-usage';
+export * from './with-value-and-units';
+export * from './with-icon-colors';
 export * from './with-channelValue-children';
-export * from './in-a-banner';
+export * from './within-a-HeroBanner';
 
 export default heroModule;
--- demos/storybook/stories/hero/with-channelValue-children.tsx
+++ demos/storybook/stories/hero/with-channelValue-children.tsx
@@ -1,16 +1,14 @@
 import * as Colors from '@pxblue/colors';
-//@ts-ignore
-import { Leaf } from '@pxblue/icons-mui';
-import { ChannelValue, Hero } from '@pxblue/react-components';
-
-import { number, text } from '@storybook/addon-knobs';
-import { StoryFnReactReturnType } from '@storybook/react/dist/client/preview/types';
+import {Leaf} from '@pxblue/icons-mui';
+import {ChannelValue, Hero} from '@pxblue/react-components';
+import {number} from '@storybook/addon-knobs';
+import {StoryFnReactReturnType} from '@storybook/react/dist/client/preview/types';
 import React from 'react';
 
 export const withChannelValueChildren = (): StoryFnReactReturnType => (
-    <Hero label={text('label', 'Duration')} icon={<Leaf fontSize={'inherit'} htmlColor={Colors.green[500]} />}>
-        <ChannelValue fontSize={'large'} value={number('hours', 1)} units={'h'} />
-        <ChannelValue fontSize={'large'} value={number('minutes', 27)} units={'m'} />
+    <Hero label={'Duration'} icon={<Leaf fontSize={'inherit'} htmlColor={Colors.green[500]} />}>
+        <ChannelValue fontSize={'large'} value={number('ChannelValue.hours', 1)} units={'h'} />
+        <ChannelValue fontSize={'large'} value={number('ChannelValue.minutes', 27)} units={'m'} />
     </Hero>
 );
 
