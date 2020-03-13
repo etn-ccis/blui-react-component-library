0dac955e0f42b9ee2e1bfd1598e1524d1ed1a0b1
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-03-13 09:03:13 -0400

    Add knob to icon story for ChannelValue

--- demos/storybook/stories/channel-value/with-icon.tsx
+++ demos/storybook/stories/channel-value/with-icon.tsx
@@ -1,11 +1,12 @@
 import Trend from '@material-ui/icons/TrendingUp';
 import * as Colors from '@pxblue/colors';
 import { ChannelValue } from '@pxblue/react-components';
+import {color} from "@storybook/addon-knobs";
 import { StoryFnReactReturnType } from '@storybook/react/dist/client/preview/types';
 import React from 'react';
 
 export const withIcon = (): StoryFnReactReturnType => (
-    <ChannelValue value={'123'} units={'hz'} icon={<Trend htmlColor={Colors.red[500]} />} />
+    <ChannelValue value={'123'} units={'hz'} icon={<Trend htmlColor={color('icon.htmlColor', Colors.red[500])} />} />
 );
 
 withIcon.story = { name: 'with icon' };
