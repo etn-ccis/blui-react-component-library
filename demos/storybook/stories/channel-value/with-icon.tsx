a1aaae85fb35c20dc1576814034e2d21e1b530df
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-03-13 09:03:41 -0400

    Run prettier again

--- demos/storybook/stories/channel-value/with-icon.tsx
+++ demos/storybook/stories/channel-value/with-icon.tsx
@@ -1,7 +1,7 @@
 import Trend from '@material-ui/icons/TrendingUp';
 import * as Colors from '@pxblue/colors';
 import { ChannelValue } from '@pxblue/react-components';
-import {color} from "@storybook/addon-knobs";
+import { color } from '@storybook/addon-knobs';
 import { StoryFnReactReturnType } from '@storybook/react/dist/client/preview/types';
 import React from 'react';
 
