183d20a3af1816fcdb40f4daae183380fdaf9798
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-03-17 09:09:15 -0400

    Add class overrides to DrawerSubheader

--- components/src/core/Drawer/DrawerSubheader.tsx
+++ components/src/core/Drawer/DrawerSubheader.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
 import { Divider } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import React from 'react';
 
+type DrawerSubheaderClasses = {
+    root?: string;
+};
 export type DrawerSubheaderProps = {
+    classes: DrawerSubheaderClasses;
     open?: boolean;
 };
 
 export const DrawerSubheader: React.FC<DrawerSubheaderProps> = (props) => (
     <>
-        <div style={{ visibility: props.open ? 'inherit' : 'hidden' }}>{props.children}</div>
+        <div style={{ visibility: props.open ? 'inherit' : 'hidden' }} className={props.classes.root}>
+            {props.children}
+        </div>
         <Divider />
     </>
 );
@@ -16,5 +22,12 @@ export const DrawerSubheader: React.FC<DrawerSubheaderProps> = (props) => (
 DrawerSubheader.displayName = 'DrawerSubheader';
 
 DrawerSubheader.propTypes = {
+    classes: PropTypes.shape({
+        root: PropTypes.string,
+    }),
     open: PropTypes.bool,
+};
+
+DrawerSubheader.defaultProps = {
+    classes: {},
 };
