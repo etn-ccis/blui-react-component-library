721552b765698bafea4c0c216bbce905ee75786f
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-03-20 12:41:15 -0400

    Revert this

--- demos/showcase/src/App.js
+++ demos/showcase/src/App.js
@@ -46,20 +46,8 @@ import {
 import top from './topology_40.png';
 import EatonLogo from './EatonLogo.svg';
 import Avatar from '@material-ui/core/Avatar';
-import {makeStyles} from "@material-ui/styles";
 
 const locations = ['All Locations', 'Gary Steelworks', 'Semaine Prochaine'];
-
-
-// @ts-ignore
-const useStyles = makeStyles({
-    nestedListGroup: {
-        backgroundColor: 'red!important',
-        fontWeight: '200!important',
-        fontSize: '14px!important',
-        color: 'white!important'
-    },
-});
 
 export default () => {
     const [open, setOpen] = useState(false);
@@ -115,8 +103,6 @@ export default () => {
                     </DrawerSubheader>
                     <DrawerBody>
                         <DrawerNavGroup
-
-                            classes={{nestedListGroup: classes.nestedListGroup}}
                             items={[
                                 {
                                     title: titleList[0],
