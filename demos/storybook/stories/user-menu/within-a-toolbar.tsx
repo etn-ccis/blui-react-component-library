54ece4e8d0da47a97ab35745f57065e2f9be5dda
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-03-23 12:58:27 -0400

    Start supporting non clickable items

--- demos/storybook/stories/user-menu/within-a-toolbar.tsx
+++ demos/storybook/stories/user-menu/within-a-toolbar.tsx
@@ -1,19 +1,25 @@
 import { AppBar, Avatar, Toolbar, Typography } from '@material-ui/core';
-import { Spacer, UserMenu } from '@pxblue/react-components';
+import {Spacer, UserMenu, UserMenuGroup} from '@pxblue/react-components';
 import { action } from '@storybook/addon-actions';
 import { StoryFnReactReturnType } from '@storybook/react/dist/client/preview/types';
 import React from 'react';
 import { menuGroups } from './with-basic-usage';
 import * as Colors from '@pxblue/colors';
 
-export const withinToolbar = (): StoryFnReactReturnType => (
-    <div style={{ width: '80%', height: 150 }}>
-        <AppBar position={'relative'} color={'primary'} style={{ marginTop: '-32px' }}>
-            <Toolbar style={{ padding: '0 16px' }}>
+export const withinToolbar = (): StoryFnReactReturnType => {
+    const group: UserMenuGroup = Object.assign({}, menuGroups[0]);
+    group.items.push({
+        itemID: '4',
+        title: 'Project Dixie',
+        subtitle: 'v1.2'
+    });
+   return <div style={{width: '80%', height: 150}}>
+        <AppBar position={'relative'} color={'primary'} style={{marginTop: '-32px'}}>
+            <Toolbar style={{padding: '0 16px'}}>
                 <Typography variant={'h6'}>Toolbar Title</Typography>
-                <Spacer flex={1} />
+                <Spacer flex={1}/>
                 <UserMenu
-                    avatar={<Avatar />}
+                    avatar={<Avatar/>}
                     menuGroups={menuGroups}
                     menuTitle={'Jane Doe'}
                     menuSubtitle={'Account Manager'}
@@ -22,10 +28,10 @@ export const withinToolbar = (): StoryFnReactReturnType => (
                 />
             </Toolbar>
         </AppBar>
-        <div style={{ height: '100%', backgroundColor: Colors.white[50], padding: 16 }}>
+        <div style={{height: '100%', backgroundColor: Colors.white[50], padding: 16}}>
             <Typography variant={'subtitle1'}>Body Content Goes Here</Typography>
         </div>
     </div>
-);
+};
 
 withinToolbar.story = { name: 'within a toolbar' };
