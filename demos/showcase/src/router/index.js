f660d5a0f72765d9d6cfeafadb8eb26443a2230e
Evan McLaughlin <evanmclaughlin@eaton.com>
2020-03-23 09:31:28 -0400

    Update demos/showcase/src/router/index.js
    Co-Authored-By: Joseph Boyle <boyle.p.joseph@gmail.com>

--- demos/showcase/src/router/index.js
+++ demos/showcase/src/router/index.js
@@ -5,10 +5,21 @@ import { NavigationDrawer } from './NavigationDrawer';
 import { App } from '../App';
 import { SharedAppBar } from '../components/SharedAppBar';
 
+const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+}
+
 export const MainRouter = () => {
     const [open, setOpen] = useState(true);
     return (
         <Router>
+            <ScrollToTop/>
             <DrawerLayout drawer={<NavigationDrawer open={open} setOpen={setOpen} />}>
                 <Switch>
                     <Route path="*">
