9a242c5604f01918bc53d9e82e2374bae3950fe3
daileytj <daileytj89@gmail.com>
2020-08-19 14:01:35 -0400

    Flip icons when direction is rtl

--- demos/storybook/stories/dropdown-toolbar/with-custom-menu.tsx
+++ demos/storybook/stories/dropdown-toolbar/with-custom-menu.tsx
@@ -5,20 +5,23 @@ import { AppBar, Menu } from '@material-ui/core';
 import { Business, House, Apartment } from '@material-ui/icons';
 import * as Colors from '@pxblue/colors';
 import { action } from '@storybook/addon-actions';
+import { getDirection } from '@pxblue/storybook-rtl-addon';
 
 export const withCustomMenu = (): StoryFnReactReturnType => {
+    const direction = getDirection();
+
     const menu = (): JSX.Element => (
         <Menu open={true}>
             <InfoListItem
                 title={'Atlanta'}
-                icon={<Business />}
+                icon={<Business style={{ transform: direction === 'rtl' ? 'scaleX(-1)' : '' }} />}
                 iconColor={Colors.blue[500]}
                 onClick={action('Atlanta selected')}
                 dense
             ></InfoListItem>
             <InfoListItem
                 title={'Pittsburgh'}
-                icon={<House />}
+                icon={<House style={{ transform: direction === 'rtl' ? 'scaleX(-1)' : '' }} />}
                 iconColor={Colors.red[500]}
                 statusColor={Colors.red[500]}
                 onClick={action('Pittsburgh selected')}
@@ -26,7 +29,7 @@ export const withCustomMenu = (): StoryFnReactReturnType => {
             ></InfoListItem>
             <InfoListItem
                 title={'New York'}
-                icon={<Apartment />}
+                icon={<Apartment style={{ transform: direction === 'rtl' ? 'scaleX(-1)' : '' }} />}
                 iconColor={Colors.blue[500]}
                 onClick={action('New York selected')}
                 dense
