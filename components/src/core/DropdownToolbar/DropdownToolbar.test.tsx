aba4c0f127312b0d8d87e186f5c68be61c449786
daileytj <daileytj89@gmail.com>
2020-08-19 11:37:40 -0400

    Add open menu test

--- components/src/core/DropdownToolbar/DropdownToolbar.test.tsx
+++ components/src/core/DropdownToolbar/DropdownToolbar.test.tsx
@@ -5,6 +5,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import { DropdownToolbar } from './DropdownToolbar';
 import { ListItemText, Typography } from '@material-ui/core';
 import { Menu } from '@material-ui/icons';
+import { act } from 'react-dom/test-utils';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -29,5 +30,30 @@ describe('DropdownToolbar', () => {
         const icon = <Menu />;
         const toolbar = Enzyme.mount(<DropdownToolbar title={'title'} subtitle={'Hello'} navigationIcon={icon} />);
         expect(toolbar.find(Menu).length).toEqual(1);
+    });
+
+    it('should open menu when subtitle is clicked', () => {
+        const menuItems = [{ title: 'Item 1', onClick: (): void => {}, itemID: 'item1' }];
+
+        const menuGroups = [
+            {
+                items: menuItems,
+            },
+        ];
+
+        const onOpen = jest.fn();
+        const toolbar = Enzyme.mount(
+            <DropdownToolbar title={'title'} subtitle={'Hello'} onOpen={onOpen} menuGroups={menuGroups} />
+        );
+        const listItemText = toolbar.find(ListItemText);
+        expect(listItemText.length).toEqual(1);
+        const typography = listItemText.find(Typography);
+        expect(typography.length).toEqual(2);
+        const subtitleWithDropdownArrow = typography.get(1);
+        expect(onOpen).not.toHaveBeenCalled();
+        act(() => {
+            subtitleWithDropdownArrow.props.onClick();
+        });
+        expect(onOpen).toHaveBeenCalled();
     });
 });
