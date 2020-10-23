a3bc987680be9f84cd022ea8537154ee8db5312d
joebochill <boyle.p.joseph@gmail.com>
2020-10-23 10:44:07 -0400

    Fix LIT margin for RTL in storybook

--- demos/storybook/stories/list-item-tag/within-InfoListItem.tsx
+++ demos/storybook/stories/list-item-tag/within-InfoListItem.tsx
@@ -4,26 +4,39 @@ import { StoryFnReactReturnType } from '@storybook/react/dist/client/preview/typ
 import React from 'react';
 import { BrightnessMedium } from '@material-ui/icons';
 import List from '@material-ui/core/List';
+import { getDirection } from '@pxblue/storybook-rtl-addon';
+import { useTheme } from '@material-ui/core';
 
-export const inInfoListItem = (): StoryFnReactReturnType => (
-    <List style={{ width: '80%', background: Colors.white[50], padding: 0 }}>
-        <InfoListItem
-            icon={<BrightnessMedium />}
-            title={'@pxblue/react-themes'}
-            subtitle={'Light and dark themes supported'}
-            rightComponent={
-                <div>
-                    <ListItemTag
-                        label={'Build Passing'}
-                        backgroundColor={Colors.green[300]}
-                        fontColor={Colors.black[900]}
-                        style={{ marginRight: 8 }}
-                    />
-                    <ListItemTag label={'5 Bugs'} backgroundColor={Colors.yellow[500]} fontColor={Colors.black[900]} />
-                </div>
-            }
-        />
-    </List>
-);
+export const inInfoListItem = (): StoryFnReactReturnType => {
+    const direction = getDirection();
+    const theme = useTheme();
+    return (
+        <List style={{ width: '80%', background: Colors.white[50], padding: 0 }}>
+            <InfoListItem
+                icon={<BrightnessMedium />}
+                title={'@pxblue/react-themes'}
+                subtitle={'Light and dark themes supported'}
+                rightComponent={
+                    <div>
+                        <ListItemTag
+                            label={'Build Passing'}
+                            backgroundColor={Colors.green[300]}
+                            fontColor={Colors.black[900]}
+                            style={{
+                                marginRight: direction === 'rtl' ? 0 : theme.spacing(1),
+                                marginLeft: direction === 'rtl' ? theme.spacing(1) : 0,
+                            }}
+                        />
+                        <ListItemTag
+                            label={'5 Bugs'}
+                            backgroundColor={Colors.yellow[500]}
+                            fontColor={Colors.black[900]}
+                        />
+                    </div>
+                }
+            />
+        </List>
+    );
+};
 
 inInfoListItem.story = { name: 'within an Info List Item' };
