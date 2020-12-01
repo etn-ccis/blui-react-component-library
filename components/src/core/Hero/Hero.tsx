1310b14ebaacc1bfe30afe2fb335067dd506d140
huayunh <hhuayun0828@gmail.com>
2020-11-30 21:51:23 -0500

    Update magic number

--- components/src/core/Hero/Hero.tsx
+++ components/src/core/Hero/Hero.tsx
@@ -46,7 +46,7 @@ const useStyles = makeStyles((theme: Theme) =>
         icon: {
             lineHeight: 1,
             color: theme.palette.text.secondary,
-            marginBottom: 5,
+            marginBottom: theme.spacing(0.5),
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
