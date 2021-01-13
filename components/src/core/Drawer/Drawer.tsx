bdf86605ff49f94d375a7f450a811072711da071
Firm-Soft <evan.w.mclaughlin@gmail.com>
2021-01-13 13:01:12 -0500

    Add openOnHover to Drawer

--- components/src/core/Drawer/Drawer.tsx
+++ components/src/core/Drawer/Drawer.tsx
@@ -142,6 +142,9 @@ export type DrawerComponentProps = {
     // Controls the open/closed state of the drawer
     open: boolean;
 
+    // Enables Drawer to automatically open on hover for persistent variants.
+    openOnHover?: boolean;
+
     // Toggles the drawer side border instead of a drop shadow
     sideBorder?: boolean;
 
@@ -181,6 +184,7 @@ const DrawerRenderer: React.ForwardRefRenderFunction<unknown, DrawerComponentPro
         nestedDivider,
         noLayout = false,
         open,
+        openOnHover,
         onItemSelect,
         ripple,
         sideBorder = false,
@@ -284,11 +288,15 @@ const DrawerRenderer: React.ForwardRefRenderFunction<unknown, DrawerComponentPro
                 <div
                     style={{ flexDirection: 'column', flex: '1 1 0px', display: 'flex' }}
                     onMouseEnter={(): void => {
-                        hoverDelay = setTimeout(() => setHover(true), 500);
+                        if (openOnHover) {
+                            hoverDelay = setTimeout(() => setHover(true), 500);
+                        }
                     }}
                     onMouseLeave={(): void => {
-                        clearTimeout(hoverDelay);
-                        setHover(false);
+                        if (openOnHover) {
+                            clearTimeout(hoverDelay);
+                            setHover(false);
+                        }
                     }}
                 >
                     {getSubHeader()}
@@ -379,11 +387,13 @@ DrawerComponent.propTypes = {
         paper: PropTypes.string,
     }),
     open: PropTypes.bool.isRequired,
+    openOnHover: PropTypes.bool,
     sideBorder: PropTypes.bool,
     width: PropTypes.number,
     ...PXBlueDrawerNavGroupInheritablePropertiesPropTypes,
 };
 DrawerComponent.defaultProps = {
     classes: {},
+    openOnHover: true,
     sideBorder: false,
 };
