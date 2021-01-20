e2bee4c7b761a0fb073d49b88a522c4d0550f12d
emclaug2 <evan.w.mclaughlin@gmail.com>
2021-01-20 13:00:33 -0500

    Remove icon flipping

--- demos/storybook/stories/dropdown-toolbar/with-custom-menu.tsx
+++ demos/storybook/stories/dropdown-toolbar/with-custom-menu.tsx
@@ -5,35 +5,32 @@ import { AppBar, Menu } from '@material-ui/core';
 import { Business, House, Apartment } from '@material-ui/icons';
 import * as Colors from '@pxblue/colors';
 import { action } from '@storybook/addon-actions';
-import { getDirection } from '@pxblue/storybook-rtl-addon';
 
 export const withCustomMenu = (): StoryFnReactReturnType => {
-    const direction = getDirection();
-
     const menu = (): JSX.Element => (
         <Menu open={true}>
             <InfoListItem
                 title={'Atlanta'}
-                icon={<Business style={{ transform: direction === 'rtl' ? 'scaleX(-1)' : '' }} />}
+                icon={<Business />}
                 iconColor={Colors.blue[500]}
                 onClick={action('Atlanta selected')}
                 dense
-            ></InfoListItem>
+            />
             <InfoListItem
                 title={'Pittsburgh'}
-                icon={<House style={{ transform: direction === 'rtl' ? 'scaleX(-1)' : '' }} />}
+                icon={<House />}
                 iconColor={Colors.red[500]}
                 statusColor={Colors.red[500]}
                 onClick={action('Pittsburgh selected')}
                 dense
-            ></InfoListItem>
+            />
             <InfoListItem
                 title={'New York'}
-                icon={<Apartment style={{ transform: direction === 'rtl' ? 'scaleX(-1)' : '' }} />}
+                icon={<Apartment/>}
                 iconColor={Colors.blue[500]}
                 onClick={action('New York selected')}
                 dense
-            ></InfoListItem>
+            />
         </Menu>
     );
 
