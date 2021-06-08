81dd23eca9185ff14581484e19ae8cee9f7370fc
joebochill <boyle.p.joseph@gmail.com>
2021-06-08 08:59:33 -0400

    Refactor to remove continuous resize effect

--- components/src/core/AppBar/AppBar.tsx
+++ components/src/core/AppBar/AppBar.tsx
@@ -1,23 +1,30 @@
-import React, { /*useEffect,*/ useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
     AppBar as MuiAppBar,
     AppBarProps as MuiAppBarProps,
     createStyles,
     makeStyles,
+    Theme,
     useMediaQuery,
     useTheme,
 } from '@material-ui/core';
-import { useScrollPosition } from '@n8tb1t/use-scroll-position';
+// import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 import clsx from 'clsx';
+import { usePrevious } from '../hooks/usePrevious';
 
 export type AppBarClasses = {
     root?: string;
     background?: string;
 };
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             overflow: 'hidden',
+            transition: (props: AppBarProps): string =>
+                theme.transitions.create(['height'], {
+                    duration: props.animationDuration || theme.transitions.duration.standard,
+                    easing: theme.transitions.easing.easeInOut,
+                }),
         },
         background: {
             position: 'absolute',
@@ -33,6 +40,12 @@ const useStyles = makeStyles(() =>
 );
 
 export type AppBarProps = MuiAppBarProps & {
+    /**
+     * Length of the collapse / expand animation (in ms)
+     * Default: 300
+     */
+    animationDuration?: number;
+
     /**
      * Image to use as the background for the header
      * Default: none
@@ -73,6 +86,12 @@ export type AppBarProps = MuiAppBarProps & {
     mode?: 'expanded' | 'collapsed' | 'dynamic';
 
     /**
+     * How far to scroll before collapsing the app bar
+     * Default: 50
+     */
+    scrollThreshold?: number;
+
+    /**
      * A ref to the scrollable container that controls the app bar height
      * Default: window
      * TODO: NOT IMPLEMENTED YET
@@ -98,6 +117,7 @@ const AppBarRender: React.ForwardRefRenderFunction<unknown, AppBarProps> = (prop
     const {
         style = {},
         mode = 'dynamic',
+        animationDuration: durationProp,
         expandedHeight = 200,
         backgroundImage,
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -106,38 +126,54 @@ const AppBarRender: React.ForwardRefRenderFunction<unknown, AppBarProps> = (prop
         collapsedHeight: collapsedHeightProp,
         // onExpandedHeightReached,
         // onCollapsedHeightReached,
+        scrollThreshold = 100,
         ...muiAppBarProps
     } = props;
     const theme = useTheme();
     const defaultClasses = useStyles(props);
+    const animationDuration = durationProp || theme.transitions.duration.standard;
     const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
-    const [offset, setOffset] = useState(0);
-
-    // Calculate the correct App Bar size
     const defaultAppBarHeight = isMobile ? theme.spacing(7) : theme.spacing(8);
     const collapsedHeight = collapsedHeightProp || defaultAppBarHeight;
-    const calculatedHeight =
+
+    const [offset, setOffset] = useState(0);
+    const previousOffset = usePrevious(offset);
+    const [scrolling, setScrolling] = useState(false);
+    const [animating, setAnimating] = useState(false);
+    const [height, setHeight] = useState(
         mode === 'collapsed'
             ? collapsedHeight
             : mode === 'expanded'
             ? expandedHeight
-            : Math.round(Math.max(expandedHeight - offset, collapsedHeight));
-    const calculatedMargin = mode === 'dynamic' ? expandedHeight - calculatedHeight : 0;
-    const calculatedPosition =
-        mode === 'dynamic' ? (calculatedHeight <= collapsedHeight ? 'sticky' : 'relative') : props.position;
-
-    // TODO: This is inefficient...we listen for scroll events even if we are in a mode that doesn't react
-    // We may need to break this into two sub-components, one with scroll and one without since we can't
-    // conditionally execute React hooks.
-    // Update the height based on scroll position of WINDOW
-    useScrollPosition(
-        ({ currPos }) => {
-            if (mode === 'dynamic') setOffset(currPos.y);
-        },
-        [offset, setOffset, mode],
-        undefined,
-        true
+            : window.scrollY > scrollThreshold
+            ? collapsedHeight
+            : expandedHeight
     );
+
+    useEffect(() => {
+        if (animating || mode !== 'dynamic') return;
+
+        if (previousOffset < scrollThreshold && offset >= scrollThreshold) {
+            setAnimating(true);
+            setHeight(collapsedHeight);
+            setTimeout(() => {
+                setAnimating(false);
+                setScrolling(false);
+                if (window.scrollY === 0) {
+                    window.scrollTo(0, 1);
+                }
+            }, animationDuration + 50);
+        }
+        // go from small to big if we scroll back to the top
+        else if (previousOffset > 0 && offset === 0) {
+            setAnimating(true);
+            setHeight(expandedHeight);
+            setTimeout(() => {
+                setAnimating(false);
+                setScrolling(false);
+            }, animationDuration + 50);
+        }
+    }, [animationDuration, animating, previousOffset, collapsedHeight, expandedHeight, offset, scrollThreshold]);
 
     const getBackgroundImage = useCallback((): JSX.Element | undefined => {
         if (backgroundImage) {
@@ -145,43 +181,37 @@ const AppBarRender: React.ForwardRefRenderFunction<unknown, AppBarProps> = (prop
         }
     }, [backgroundImage, defaultClasses, classes]);
 
-    // Handle callback functions when reaching the extremes of dynamic app bar
-    // useEffect(() => {
-    //     if (mode !== 'dynamic') return;
-    //     if (calculatedHeight >= expandedHeight && onExpandedHeightReached) onExpandedHeightReached();
-    //     if (calculatedHeight <= collapsedHeight && onCollapsedHeightReached) onCollapsedHeightReached();
-    // }, [mode, calculatedHeight, expandedHeight, collapsedHeight, onExpandedHeightReached, onCollapsedHeightReached]);
+    const handleScroll = useCallback(() => {
+        if (scrolling && !animating) {
+            setScrolling(false);
+            if (window.scrollY !== offset) {
+                setOffset(window.scrollY);
+            }
+        }
+    }, [scrolling, animating, offset]);
 
-    /*
-    // This works for supporting a custom element scroller, but it's not very pretty
-    // const handleScroll = useCallback((e: any) => {
-    //     console.log('scrolling:', scrollRef?.current?.scrollTop);
-    //     setOffset(scrollRef?.current?.scrollTop);
-    // }, [setOffset, scrollRef, scrollRef?.current]);
-    //
-    // useEffect(() => {
-    //     if (scrollRef && scrollRef.current) {
-    //         // @ts-ignore
-    //         scrollRef?.current?.addEventListener('scroll', handleScroll);
-    //         // scrollRef?.current?.addEventListener('scroll', (e) => {
-    //         //     setOffset(scrollRef?.current?.scrollTop);
-    //         // });
-    //         return () => scrollRef?.current?.removeEventListener('scroll', handleScroll);
-    //     }
-    // }, [scrollRef, scrollRef?.current]);
-    */
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            setScrolling(true);
+        });
+        const scrollCheck = setInterval(handleScroll, 50);
+        return (): void => {
+            clearInterval(scrollCheck);
+            window.removeEventListener('scroll', () => setScrolling(true));
+        };
+    }, [handleScroll]);
 
     return (
         <>
             <MuiAppBar
                 ref={ref}
                 {...muiAppBarProps}
+                className={clsx(defaultClasses.root, classes.root)}
                 style={Object.assign({}, style, {
-                    height: calculatedHeight,
-                    marginTop: calculatedMargin,
+                    height: height,
                     overflow: 'hidden',
                 })}
-                position={calculatedPosition}
+                position={'sticky'}
             >
                 {getBackgroundImage()}
                 {props.children}
@@ -191,3 +221,56 @@ const AppBarRender: React.ForwardRefRenderFunction<unknown, AppBarProps> = (prop
 };
 export const AppBar = React.forwardRef(AppBarRender);
 AppBar.displayName = 'AppBar';
+
+/* Additional code from the work on the continuous scrolling app bar that worked
+but did ot perform well with complex toolbars or applications. */
+
+// Calculate the correct App Bar size
+// const defaultAppBarHeight = isMobile ? theme.spacing(7) : theme.spacing(8);
+// const collapsedHeight = collapsedHeightProp || defaultAppBarHeight;
+// const calculatedHeight =
+//     mode === 'collapsed'
+//         ? collapsedHeight
+//         : mode === 'expanded'
+//             ? expandedHeight
+//             : Math.round(Math.max(expandedHeight - offset, collapsedHeight));
+// const calculatedMargin = mode === 'dynamic' ? expandedHeight - calculatedHeight : 0;
+// const calculatedPosition =
+//     mode === 'dynamic' ? (calculatedHeight <= collapsedHeight ? 'sticky' : 'relative') : props.position;
+
+// TODO: This is inefficient...we listen for scroll events even if we are in a mode that doesn't react
+// We may need to break this into two sub-components, one with scroll and one without since we can't
+// conditionally execute React hooks.
+// Update the height based on scroll position of WINDOW
+// useScrollPosition(
+//     ({ currPos }) => {
+//         if (mode === 'dynamic') setOffset(currPos.y);
+//     },
+//     [offset, setOffset, mode],
+//     undefined,
+//     true
+// );
+
+// Handle callback functions when reaching the extremes of dynamic app bar
+// useEffect(() => {
+//     if (mode !== 'dynamic') return;
+//     if (calculatedHeight >= expandedHeight && onExpandedHeightReached) onExpandedHeightReached();
+//     if (calculatedHeight <= collapsedHeight && onCollapsedHeightReached) onCollapsedHeightReached();
+// }, [mode, calculatedHeight, expandedHeight, collapsedHeight, onExpandedHeightReached, onCollapsedHeightReached]);
+
+// This works for supporting a custom element scroller, but it's not very pretty
+// const handleScroll = useCallback((e: any) => {
+//     console.log('scrolling:', scrollRef?.current?.scrollTop);
+//     setOffset(scrollRef?.current?.scrollTop);
+// }, [setOffset, scrollRef, scrollRef?.current]);
+//
+// useEffect(() => {
+//     if (scrollRef && scrollRef.current) {
+//         // @ts-ignore
+//         scrollRef?.current?.addEventListener('scroll', handleScroll);
+//         // scrollRef?.current?.addEventListener('scroll', (e) => {
+//         //     setOffset(scrollRef?.current?.scrollTop);
+//         // });
+//         return () => scrollRef?.current?.removeEventListener('scroll', handleScroll);
+//     }
+// }, [scrollRef, scrollRef?.current]);
--- docs/AppBar.md
+++ docs/AppBar.md
@@ -1,6 +1,6 @@
 # Dropdown Toolbar
 
-The `<AppBar>` component is an extension of the default AppBar from Material UI that can be resized as the page is scrolled. It supports three modes:
+The `<AppBar>` component is an extension of the default AppBar from Material UI that can be resized / collapsed as the page is scrolled. It supports three modes:
 
 <div style="width: 100%; text-align: center">
     <img width="100%" style="max-width: 600px" alt="Dropdown Toolbar" src="./images/appBar.png">
@@ -32,14 +32,16 @@ import { AppBar } from '@pxblue/react-components';
 
 <div style="overflow: auto;">
 
-| Prop Name              | Description                                  | Type                                         | Required | Default       |
-| ---------------------- | -------------------------------------------- | -------------------------------------------- | -------- | ------------- |
-| backgroundImage        | Image to use as the app bar background       | `string`                                     | no       |               |
-| backgroundImageOpacity | Opacity to use to blend the background image | `number`                                     | no       | 0.3           |
-| classes                | Style Overrides                              | `AppBarClasses`                              | no       |               |
-| collapsedHeight        | Height of the AppBar when collapsed          | `number`                                     | no       | theme default |
-| expandedHeight         | Height of the AppBar when expanded           | `number`                                     | no       | 200           |
-| mode                   | Behavior mode of the App Bar                 | `'expanded'` \| `'collapsed'` \| `'dynamic'` | no       | 'dynamic'     |
+| Prop Name              | Description                                      | Type                                         | Required | Default       |
+| ---------------------- | ------------------------------------------------ | -------------------------------------------- | -------- | ------------- |
+| animationDuration      | Length of the collapse / expand animation (ms)   | `number`                                     | no       | theme default |
+| backgroundImage        | Image to use as the app bar background           | `string`                                     | no       |               |
+| backgroundImageOpacity | Opacity to use to blend the background image     | `number`                                     | no       | 0.3           |
+| classes                | Style Overrides                                  | `AppBarClasses`                              | no       |               |
+| collapsedHeight        | Height of the AppBar when collapsed              | `number`                                     | no       | theme default |
+| expandedHeight         | Height of the AppBar when expanded               | `number`                                     | no       | 200           |
+| mode                   | Behavior mode of the App Bar                     | `'expanded'` \| `'collapsed'` \| `'dynamic'` | no       | 'dynamic'     |
+| scrollThreshold        | Distance to scroll before collapsing the app bar | `number`                                     | no       | 100           |
 
 </div>
 
