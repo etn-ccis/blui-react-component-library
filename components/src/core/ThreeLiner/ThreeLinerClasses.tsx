59300fa2ee77474006bdc733e550d478e43e2c45
Ekta Ghag <ektasawant99@gmail.com>
2022-04-21 19:08:32 +0530

    use styled engine for threeliner component

---
+++ components/src/core/ThreeLiner/ThreeLinerClasses.tsx
@@ -0,0 +1,29 @@
+import { generateUtilityClass, generateUtilityClasses } from '@mui/base';
+
+export type ThreeLinerClasses = {
+    /** Styles applied to the root element. */
+    root?: string;
+    /** First Line Content */
+    title?: string;
+
+    /** Second Line Content */
+    subtitle?: string;
+
+    /** Third Line Content */
+    info?: string;
+};
+
+export type ThreeLinerClassKey = keyof ThreeLinerClasses;
+
+export function getThreeLinerUtilityClass(slot: string): string {
+    return generateUtilityClass('BluiThreeLiner', slot);
+}
+
+const threeLinerClasses: ThreeLinerClasses = generateUtilityClasses('BluiThreeLiner', [
+    'root',
+    'title',
+    'subtitle',
+    'info',
+]);
+
+export default threeLinerClasses;
