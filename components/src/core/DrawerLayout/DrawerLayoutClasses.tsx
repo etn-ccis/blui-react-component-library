8dc3e4023acc25f87901eb79e77cf6f4f616e6d3
daileytj <daileytj89@gmail.com>
2022-04-27 12:56:51 -0400

    Update drawer to extend sx prop

---
+++ components/src/core/DrawerLayout/DrawerLayoutClasses.tsx
@@ -0,0 +1,27 @@
+import { generateUtilityClass, generateUtilityClasses } from '@mui/base';
+
+export type DrawerLayoutClasses = {
+    /** Styles applied to the root element */
+    root?: string;
+    /** Styles applied to the body content container */
+    content?: string;
+    /** Styles applied to the drawer container */
+    drawer?: string;
+    /** Styles applied to the body root element when the drawer is expanded */
+    expanded?: string;
+};
+
+export type DrawerLayoutClassKey = keyof DrawerLayoutClasses;
+
+export function getDrawerLayoutUtilityClass(slot: string): string {
+    return generateUtilityClass('BluiDrawerLayout', slot);
+}
+
+const drawerLayoutClasses: DrawerLayoutClasses = generateUtilityClasses('BluiDrawerLayout', [
+    'root',
+    'content',
+    'drawer',
+    'expanded',
+]);
+
+export default drawerLayoutClasses;
