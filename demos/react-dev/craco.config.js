0a4fe359dc4c057eb7eb3e04eb8b36053bae454f
Suraj Karambe <bkarambe@gmail.com>
2022-07-20 15:14:47 +0530

    Added remark-gfm dep and updated craco config

--- demos/react-dev/craco.config.js
+++ demos/react-dev/craco.config.js
@@ -1,12 +1,22 @@
 const { addAfterLoader, loaderByName } = require('@craco/craco');
 
+const remarkGfm = require('remark-gfm');
+
 module.exports = {
     webpack: {
         configure(webpackConfig) {
             addAfterLoader(webpackConfig, loaderByName('babel-loader'), {
                 test: /\.mdx?$/,
-                loader: require.resolve('@mdx-js/loader'),
+
+                loader: '@mdx-js/loader',
+
+                options: {
+                    providerImportSource: '@mdx-js/react',
+
+                    remarkPlugins: [remarkGfm],
+                },
             });
+
             return webpackConfig;
         },
     },
