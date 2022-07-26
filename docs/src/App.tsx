6607b936a412ec7e6e2d3eda9529dc07981e63c6
Ekta Ghag <ektasawant99@gmail.com>
2022-07-26 11:44:49 +0530

    add babel plugin

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -4,6 +4,11 @@ import { NavigationDrawer } from './router/drawer';
 import { MainRouter } from './router/main';
 import { DrawerLayout } from '@brightlayer-ui/react-components';
 import { Routes } from 'react-router-dom';
+import "prismjs/themes/prism-tomorrow.css";
+// import 'prismjs/themes/prism-okaidia.css'
+import 'prismjs/components/prism-jsx.js'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
 export const App = (): JSX.Element => {
     const [drawerOpen, setDrawerOpen] = useState(false);
--- docs/src/components/componentList/drawerNavItemComponent/DrawerNavItemExamplesComponent.tsx
+++ docs/src/components/componentList/drawerNavItemComponent/DrawerNavItemExamplesComponent.tsx
@@ -2,10 +2,31 @@ import React from 'react';
 import { EmptyState } from '@brightlayer-ui/react-components';
 import { Box } from '@mui/material';
 import ConstructionIcon from '@mui/icons-material/Construction';
+import BasicDrawerNavItem from '../../../examples/drawerNavItem/BasicDrawerNavItem';
+const JSCode = `const App = props => {
+    return (
+      <div>
+        <h1> Prism JS </h1>
+        <div>Awesome Syntax Highlighter</div>
+      </div>
+    );
+  };
+  `;
+  
+  const htmlCode = `
+      <div>
+        <h1> PrismJS Tutorial </h1>
+        <p>
+        Prism is a lightweight, extensible syntax highlighter, built with modern web standards in mind.
+        </p>
+      </div>
+  `;
 export const DrawerNavItemExamplesComponent = (): JSX.Element => {
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-            <EmptyState icon={<ConstructionIcon fontSize={'inherit'} />} title={'Examples for drawer nav item coming soon'} />
+            {/* <EmptyState icon={<ConstructionIcon fontSize={'inherit'} />} title={'Examples for drawer nav item coming soon'} /> */}
+            <BasicDrawerNavItem code={JSCode} language="javascript" />
+            <BasicDrawerNavItem code={htmlCode} language="html" />
         </Box>
     );
 };
