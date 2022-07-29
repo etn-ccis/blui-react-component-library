0568a57acd8b9dc8f2b0d984a1d1ea8bd72a5d43
Ekta Ghag <ektasawant99@gmail.com>
2022-07-29 10:15:13 +0530

    remove duplicate code

--- docs/src/components/componentList/drawerNavItemComponent/BasicDrawerNavItem.tsx
+++ docs/src/components/componentList/drawerNavItemComponent/BasicDrawerNavItem.tsx
@@ -5,17 +5,17 @@ import { Drawer, DrawerBody, DrawerNavGroup, DrawerNavItem } from '@brightlayer-
 import * as colors from '@brightlayer-ui/colors';
 
 const htmlCode = `<DrawerBody>
-  <DrawerNavGroup>
-    <DrawerNavItem title="Dashboard" icon={<Dashboard/>} itemID="1" />
-    <DrawerNavItem title="Locations" icon={<Notifications />} itemID="2" />
-    <DrawerNavItem title="Legal" icon={<Gavel />} itemID="3" />
-  </DrawerNavGroup>
+    <DrawerNavGroup hidePadding divider>
+        <DrawerNavItem title="Dashboard" itemID="1" />
+        <DrawerNavItem title="Locations" itemID="2" />
+        <DrawerNavItem title="Legal" itemID="3" />
+    </DrawerNavGroup>
 </DrawerBody>
 `;
 export const BasicDrawerNavItem = (): JSX.Element => {
     return (
         <Box>
-            <Box sx={{ m: '32px 0', backgroundColor: colors.white[600], p: 4 }}>
+            <Box sx={{ m: '16px 0', backgroundColor: colors.white[600], p: 4 }}>
                 <Drawer open={true} width={250} sx={{ height: '156px', margin: '0 auto' }} noLayout>
                     <DrawerBody>
                         <DrawerNavGroup hidePadding divider>
