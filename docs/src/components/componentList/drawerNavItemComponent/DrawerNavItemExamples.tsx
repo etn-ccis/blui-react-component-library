e5beb9e4bd1f4c915b9c47f3895d299e8533bb95
Ekta Ghag <ektasawant99@gmail.com>
2022-07-29 13:36:28 +0530

    remove example component file

--- docs/src/components/componentList/drawerNavItemComponent/DrawerNavItemExamples.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import DrawerNavItemDocs from '../../../markdown/examples/DrawerNavItemDocs.mdx';
-
-export const DrawerNavItemExamples = (): JSX.Element => {
-    return <DrawerNavItemDocs />;
-};
--- docs/src/components/componentList/drawerNavItemComponent/index.tsx
+++ docs/src/components/componentList/drawerNavItemComponent/index.tsx
@@ -2,4 +2,3 @@ export * from './BasicDrawerNavItem';
 export * from './DrawerNavItemWithIcons';
 export * from './DrawerNestedDrawerItems';
 export * from './DrawerSelectedDrawerItems';
-export * from './DrawerNavItemExamples';
