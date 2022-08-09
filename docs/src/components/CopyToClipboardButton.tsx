4b233884f979bf146b76b20f2021f43dfedebf84
Suraj Karambe <bkarambe@gmail.com>
2022-08-09 15:28:55 +0530

    Review chages - added tooltipProps, removed copiedPosition

--- docs/src/components/CopyToClipboardButton.tsx
+++ docs/src/components/CopyToClipboardButton.tsx
@@ -7,54 +7,35 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
 type Position = 'bottom' | 'top' | 'left' | 'right';
-type CopyToClipboardProps = Omit<
-    Omit<Omit<Omit<Omit<Omit<TooltipProps, 'placement'>, 'open'>, 'children'>, 'onMouseEnter'>, 'onMouseLeave'>,
-    'title'
-> & {
+type CopyToClipboardProps = {
     duration?: number;
     position?: Position;
     title?: string;
-    copiedPosition?: Position;
     copiedTitle?: string;
     copyText: string;
+    toolTipProps?: TooltipProps;
 };
+
 export const CopyToClipboard: React.FC<CopyToClipboardProps> = (props) => {
-    const {
-        duration = 1000,
-        position = 'bottom',
-        title = '',
-        copiedPosition = 'bottom',
-        copiedTitle = 'Copied',
-        copyText,
-        ...otherProps
-    } = props;
+    const { duration = 1000, position = 'bottom', title = '', copiedTitle = 'Copied', copyText, toolTipProps } = props;
     const [isCopied, setIsCopied] = useState(false);
     const [showTooltip, setShowTooltip] = useState(false);
 
     return isMobile ? null : (
         <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Tooltip
-                {...otherProps}
-                title={isCopied ? copiedTitle : title}
-                placement={isCopied ? copiedPosition : position}
-                open={showTooltip}
-            >
+            <Tooltip {...toolTipProps} title={isCopied ? copiedTitle : title} placement={position} open={showTooltip}>
                 <Button
                     variant="outlined"
-                    startIcon={
-                        <CopyAllIcon
-                            sx={{
-                                fontSize: 16,
-                                cursor: 'pointer',
-                            }}
-                            onMouseEnter={(): void => {
-                                if (!isCopied) setShowTooltip(true);
-                            }}
-                            onMouseLeave={(): void => {
-                                if (!isCopied) setShowTooltip(false);
-                            }}
-                        />
-                    }
+                    sx={{
+                        fontSize: 16,
+                        cursor: 'pointer',
+                    }}
+                    onMouseEnter={(): void => {
+                        if (!isCopied) setShowTooltip(true);
+                    }}
+                    onMouseLeave={(): void => {
+                        if (!isCopied) setShowTooltip(false);
+                    }}
                     onClick={(): void => {
                         copyTextToClipboard(copyText, () => {
                             setIsCopied(true);
@@ -65,6 +46,7 @@ export const CopyToClipboard: React.FC<CopyToClipboardProps> = (props) => {
                             }, duration);
                         });
                     }}
+                    startIcon={<CopyAllIcon />}
                 >
                     Copy All
                 </Button>
