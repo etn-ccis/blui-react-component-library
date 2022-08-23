f51e243d20a5dfb5449d8fab4940745dc6253c48
Ekta Ghag <ektasawant99@gmail.com>
2022-08-23 22:45:21 +0530

    update jsx code

--- docs/src/componentDocs/Drawer/playground/PreviewComponent.tsx
+++ docs/src/componentDocs/Drawer/playground/PreviewComponent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import React from 'react';
 import { Box } from '@mui/material';
 import { RootState } from '../../../redux/store';
@@ -25,6 +26,7 @@ import {
     Toc,
     NotificationsActive,
 } from '@mui/icons-material';
+import { CodeBlock } from '../../../shared/CodeBlock';
 
 export const PreviewComponent = (): JSX.Element => {
     const drawerJson = useAppSelector((state: RootState) => state.drawerComponentData.drawerComponent);
@@ -52,17 +54,120 @@ export const PreviewComponent = (): JSX.Element => {
 
     const drawerProps: DrawerProps = createProps(drawerJson, 'Drawer');
     const drawerOtherProps = createOtherProps(drawerJson, 'Drawer');
+
+    const jsx = `
+    <Drawer
+                activeItem="${drawerProps.activeItem}"
+                activeItemBackgroundColor="${drawerOtherProps.activeItemBackgroundColor}"
+                activeItemFontColor="${drawerOtherProps.activeItemFontColor}"
+                activeItemIconColor="${drawerOtherProps.activeItemIconColor}"
+                activeItemBackgroundShape="${drawerOtherProps.activeItemBackgroundShape}"
+                chevron="${drawerOtherProps.chevron}"
+                chevronColor="${drawerOtherProps.chevronColor}"
+                // collapseIcon={drawerOtherProps.collapseIcon}
+                condensed="${drawerProps.condensed}"
+                divider="${drawerOtherProps.divider}"
+                // expandIcon={drawerOtherProps.expandIcon}
+                hidePadding="${drawerOtherProps.hidePadding}"
+                itemFontColor="${drawerOtherProps.itemFontColor}"
+                itemIconColor="${drawerOtherProps.itemIconColor}"
+                nestedBackgroundColor="${drawerOtherProps.nestedBackgroundColor}"
+                nestedDivider="${drawerOtherProps.nestedDivider}"
+                noLayout="${drawerProps.noLayout}"
+                open="${drawerProps.open}"
+                openOnHover="${drawerProps.openOnHover}"
+                openOnHoverDelay="${drawerProps.openOnHoverDelay}"
+                ripple="${drawerProps.ripple}"
+                sideBorder="${drawerProps.sideBorder}"
+                variant="${drawerProps.variant}"
+                width="${drawerProps.width}"
+            >
+                    <DrawerHeader
+                        backgroundColor={Colors.blue[500]}
+                        divider={false}
+                        fontColor={Colors.white[50]}
+                        icon={<Menu />}
+                        subtitle={'Organize your menu items here'}
+                        title={'Brightlayer UI Drawer'}
+                    ></DrawerHeader>
+                    <DrawerBody sx={{ flex: '1 1 auto' }} backgroundColor={'transparent'}>
+                        <DrawerNavGroup title={'NavGroup 1'} titleColor={Colors.black[500]} titleDivider={true}>
+                            <DrawerNavItem icon={<Toc />} itemID={'Timeline'} title={'Timeline'} />
+                            <DrawerNavItem icon={<PinDrop />} itemID={'Locations'} title={'Locations'} />
+                            <DrawerNavItem
+                                icon={<Devices />}
+                                title={'Devices'}
+                                itemID={'Devices'}
+                                subtitle={'5 new warnings'}
+                                statusColor={Colors.yellow[500]}
+                            />
+                        </DrawerNavGroup>
+                        <DrawerNavGroup title={'NavGroup 2'} titleColor={Colors.black[500]} titleDivider={true}>
+                            <DrawerNavItem icon={<MoveToInbox />} itemID={'User Guide'} title={'User Guide'} />
+                            <DrawerNavItem
+                                icon={<Send />}
+                                itemID={'License Agreement'}
+                                title={'License Agreement'}
+                                subtitle={'For Eaton employees only'}
+                            />
+                            <DrawerNavItem icon={<Accessibility />} itemID={'Accessibility'} title={'Accessibility'}>
+                                <DrawerNavItem itemID={'Color Contrast Guide'} title={'Color Contrast Guide'} />
+                                <DrawerNavItem itemID={'Screen Reader'} title={'Screen Reader'} />
+                            </DrawerNavItem>
+                            <DrawerNavItem
+                                icon={<NotificationsActive />}
+                                title={'Notifications'}
+                                itemID={'Notifications'}
+                            />
+                        </DrawerNavGroup>
+                    </DrawerBody>
+                    <DrawerFooter backgroundColor={Colors.white[50]}>
+                        <div
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                                flexDirection: 'row',
+                                padding: 16,
+                            }}
+                        >
+                            <img src={EatonFooterLogoLight} alt="Eaton Logo" height={28} width={'auto'} />
+                            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                                <Typography
+                                    variant={'caption'}
+                                >{'Copyright \u00A9 Eaton ${new Date().getFullYear()}'}</Typography>
+                                <Typography variant={'caption'}>All Rights Reserved</Typography>
+                            </div>
+                        </div>
+                    </DrawerFooter>
+                </Drawer>
+    `;
+
     return (
         <>
             <Box style={{ width: '300px' }}>
                 <Drawer
                     activeItem={drawerProps.activeItem}
                     activeItemBackgroundColor={drawerOtherProps.activeItemBackgroundColor}
+                    activeItemFontColor={drawerOtherProps.activeItemFontColor}
+                    activeItemIconColor={drawerOtherProps.activeItemIconColor}
+                    activeItemBackgroundShape={drawerOtherProps.activeItemBackgroundShape}
+                    chevron={drawerOtherProps.chevron}
+                    chevronColor={drawerOtherProps.chevronColor}
+                    // collapseIcon={drawerOtherProps.collapseIcon}
                     condensed={drawerProps.condensed}
+                    divider={drawerOtherProps.divider}
+                    // expandIcon={drawerOtherProps.expandIcon}
+                    hidePadding={drawerOtherProps.hidePadding}
+                    itemFontColor={drawerOtherProps.itemFontColor}
+                    itemIconColor={drawerOtherProps.itemIconColor}
+                    nestedBackgroundColor={drawerOtherProps.nestedBackgroundColor}
+                    nestedDivider={drawerOtherProps.nestedDivider}
                     noLayout={drawerProps.noLayout}
                     open={drawerProps.open}
                     openOnHover={drawerProps.openOnHover}
                     openOnHoverDelay={drawerProps.openOnHoverDelay}
+                    ripple={drawerProps.ripple}
                     sideBorder={drawerProps.sideBorder}
                     variant={drawerProps.variant}
                     width={drawerProps.width}
@@ -95,7 +200,10 @@ export const PreviewComponent = (): JSX.Element => {
                                 title={'License Agreement'}
                                 subtitle={'For Eaton employees only'}
                             />
-                            <DrawerNavItem icon={<Accessibility />} itemID={'Accessibility'} title={'Accessibility'} />
+                            <DrawerNavItem icon={<Accessibility />} itemID={'Accessibility'} title={'Accessibility'}>
+                                <DrawerNavItem itemID={'Color Contrast Guide'} title={'Color Contrast Guide'} />
+                                <DrawerNavItem itemID={'Screen Reader'} title={'Screen Reader'} />
+                            </DrawerNavItem>
                             <DrawerNavItem
                                 icon={<NotificationsActive />}
                                 title={'Notifications'}
@@ -124,6 +232,7 @@ export const PreviewComponent = (): JSX.Element => {
                     </DrawerFooter>
                 </Drawer>
             </Box>
+            <CodeBlock code={jsx} language="jsx" />
         </>
     );
 };
