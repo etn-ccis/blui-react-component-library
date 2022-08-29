b2dcabcb1334c1a070128948d67f0aa10fb810f6
Ekta Ghag <ektasawant99@gmail.com>
2022-08-29 11:00:07 +0530

    remove commented cde

--- docs/src/componentDocs/Drawer/playground/PlaygroundPage.tsx
+++ docs/src/componentDocs/Drawer/playground/PlaygroundPage.tsx
@@ -3,18 +3,6 @@ import { Box } from '@mui/material';
 import DrawerPlayground from './PropsPlayground';
 import { PreviewComponent } from './PreviewComponent';
 
-// const playGroundStyles = {
-//     width: '75%',
-//     height: `calc(100vh - 112px)`,
-//     display: `flex`,
-//     p: 0,
-//     overflow: `auto`,
-//     position: `relative`,
-//     alignItems: `center`,
-//     flexDirection: `column`,
-//     justifyContent: `center`,
-// };
-
 export const DrawerPlaygroundComponent = (): JSX.Element => (
     <Box>
         <Box>
