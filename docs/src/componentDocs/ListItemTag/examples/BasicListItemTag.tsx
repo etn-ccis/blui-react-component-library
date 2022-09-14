43d9c6accd6fa0bebea944b6f5a9a654df45f294
Ekta Ghag <ektasawant99@gmail.com>
2022-09-14 17:18:57 +0530

    add examples for ListItemTag

---
+++ docs/src/componentDocs/ListItemTag/examples/BasicListItemTag.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import { CodeBlock, CodeBlockActionButtonRow } from '../../../shared';
+import * as colors from '@brightlayer-ui/colors';
+import { BasicListItemTagExample } from './BasicListItemTagExample';
+
+const codeSnippet = `<ListItemTag label={'Default Tag'} />`;
+export const BasicListItemTag = (): JSX.Element => (
+    <Box>
+        <Box sx={{ m: '16px 0', backgroundColor: colors.white[600], p: 4, display: 'flex', justifyContent: 'center' }}>
+            <BasicListItemTagExample />
+        </Box>
+        <CodeBlock code={codeSnippet} language="jsx" />
+        <CodeBlockActionButtonRow
+            copyText={codeSnippet}
+            url="componentDocs/ListItemTag/examples/BasicListItemTagExample.tsx"
+        />
+    </Box>
+);
