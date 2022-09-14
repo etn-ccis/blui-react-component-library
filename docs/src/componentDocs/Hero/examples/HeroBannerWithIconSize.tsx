aa85171105c2fc4bd8395a097f0839985c67bc96
Ekta Ghag <ektasawant99@gmail.com>
2022-09-14 21:29:47 +0530

    fix wording, refactor example

--- docs/src/componentDocs/Hero/examples/HeroBannerWithIconSize.tsx
+++ docs/src/componentDocs/Hero/examples/HeroBannerWithIconSize.tsx
@@ -4,84 +4,34 @@ import { CodeBlock, CodeBlockActionButtonRow } from '../../../shared';
 import * as colors from '@brightlayer-ui/colors';
 import { HeroBannerWithIconSizeExample } from './HeroBannerWithIconSizeExample';
 
-const codeSnippet = `<HeroBanner sx={{ maxWidth: 350, m: '0 auto' }}>
-    <Hero
-        icon={<VoltageCircledOutline fontSize={'inherit'} />}
-        iconSize={36}
-        label={'SVG'}
-        ChannelValueProps={{ value: '36', units: 'px' }}
-    />
-    <Hero
-        icon={<Schedule fontSize={'inherit'} />}
-        iconSize={36}
-        label={'Mui Icon'}
-        ChannelValueProps={{ value: '36', units: 'px' }}
-    />
-    <Hero
-        icon={<i className="blui-current_circled"></i>}
-        iconSize={36}
-        label={'icon font'}
-        ChannelValueProps={{ value: '36', units: 'px' }}
-    />
-    <Hero
-        icon={<img src={Trex36} height={36} alt="A T-Rex as the avatar image" />}
-        iconSize={36}
-        label={'PNG'}
-        ChannelValueProps={{ value: '36', units: 'px' }}
-    />
-</HeroBanner>
-<HeroBanner sx={{ maxWidth: 350, m: '0 auto' }}>
-    <Hero
-        icon={<VoltageCircledOutline fontSize={'inherit'} />}
-        iconSize={48}
-        label={'SVG'}
-        ChannelValueProps={{ value: '48', units: 'px' }}
-    />
-    <Hero
-        icon={<Schedule fontSize={'inherit'} />}
-        iconSize={48}
-        label={'Mui Icon'}
-        ChannelValueProps={{ value: '48', units: 'px' }}
-    />
-    <Hero
-        icon={<i className="blui-current_circled"></i>}
-        iconSize={48}
-        label={'icon font'}
-        ChannelValueProps={{ value: '48', units: 'px' }}
-    />
-    <Hero
-        icon={<img src={Trex48} height={48} alt="A T-Rex as the avatar image" />}
-        iconSize={48}
-        label={'PNG'}
-        ChannelValueProps={{ value: '48', units: 'px' }}
-    />
-</HeroBanner>
-<HeroBanner sx={{ maxWidth: 350, m: '0 auto' }}>
-    <Hero
-        icon={<VoltageCircledOutline fontSize={'inherit'} />}
-        iconSize={72}
-        label={'SVG'}
-        ChannelValueProps={{ value: '72', units: 'px' }}
-    />
-    <Hero
-        icon={<Schedule fontSize={'inherit'} />}
-        iconSize={72}
-        label={'Mui Icon'}
-        ChannelValueProps={{ value: '72', units: 'px' }}
-    />
-    <Hero
-        icon={<i className="blui-current_circled"></i>}
-        iconSize={72}
-        label={'icon font'}
-        ChannelValueProps={{ value: '72', units: 'px' }}
-    />
-    <Hero
-        icon={<img src={Trex72} height={72} alt="A T-Rex as the avatar image" />}
-        iconSize={72}
-        label={'PNG'}
-        ChannelValueProps={{ value: '72', units: 'px' }}
-    />
-</HeroBanner>`;
+const codeSnippet = `{[36, 48, 72].map((iconSize, index) => (
+    <HeroBanner key={index}>
+        <Hero
+            icon={<VoltageCircledOutline fontSize={'inherit'} />}
+            iconSize={iconSize}
+            label={'SVG'}
+            ChannelValueProps={{ value: iconSize, units: 'px' }}
+        />
+        <Hero
+            icon={<Schedule fontSize={'inherit'} />}
+            iconSize={iconSize}
+            label={'Mui Icon'}
+            ChannelValueProps={{ value: iconSize, units: 'px' }}
+        />
+        <Hero
+            icon={<i className="blui-current_circled"></i>}
+            iconSize={iconSize}
+            label={'icon font'}
+            ChannelValueProps={{ value: iconSize, units: 'px' }}
+        />
+        <Hero
+            icon={<img src={Trex} height={iconSize} alt="A T-Rex as the avatar image" />}
+            iconSize={iconSize}
+            label={'PNG'}
+            ChannelValueProps={{ value: iconSize, units: 'px' }}
+        />
+    </HeroBanner>
+))}`;
 export const HeroBannerWithIconSize = (): JSX.Element => (
     <Box>
         <Box sx={{ m: '16px 0', backgroundColor: colors.white[600], p: 4 }}>
--- docs/src/componentDocs/Hero/examples/HeroBannerWithIconSizeExample.tsx
+++ docs/src/componentDocs/Hero/examples/HeroBannerWithIconSizeExample.tsx
@@ -5,90 +5,36 @@ import { HeroBanner } from '@brightlayer-ui/react-components/core/HeroBanner';
 import VoltageCircledOutline from '@brightlayer-ui/icons-mui/VoltageCircledOutline';
 import Schedule from '@mui/icons-material/Schedule';
 import Trex from '../images/trex.png';
+
 export const HeroBannerWithIconSizeExample = (): JSX.Element => (
     <Box sx={{ maxWidth: 350, m: '0 auto' }}>
-        {/* iconSize 36px */}
-        <HeroBanner>
-            <Hero
-                icon={<VoltageCircledOutline fontSize={'inherit'} />}
-                iconSize={36}
-                label={'SVG'}
-                ChannelValueProps={{ value: '36', units: 'px' }}
-            />
-            <Hero
-                icon={<Schedule fontSize={'inherit'} />}
-                iconSize={36}
-                label={'Mui Icon'}
-                ChannelValueProps={{ value: '36', units: 'px' }}
-            />
-            <Hero
-                icon={<i className="blui-current_circled"></i>}
-                iconSize={36}
-                label={'icon font'}
-                ChannelValueProps={{ value: '36', units: 'px' }}
-            />
-            <Hero
-                icon={<img src={Trex} height={36} alt="A T-Rex as the avatar image" />}
-                iconSize={36}
-                label={'PNG'}
-                ChannelValueProps={{ value: '36', units: 'px' }}
-            />
-        </HeroBanner>
-
-        {/* iconSize 48px */}
-        <HeroBanner>
-            <Hero
-                icon={<VoltageCircledOutline fontSize={'inherit'} />}
-                iconSize={48}
-                label={'SVG'}
-                ChannelValueProps={{ value: '48', units: 'px' }}
-            />
-            <Hero
-                icon={<Schedule fontSize={'inherit'} />}
-                iconSize={48}
-                label={'Mui Icon'}
-                ChannelValueProps={{ value: '48', units: 'px' }}
-            />
-            <Hero
-                icon={<i className="blui-current_circled"></i>}
-                iconSize={48}
-                label={'icon font'}
-                ChannelValueProps={{ value: '48', units: 'px' }}
-            />
-            <Hero
-                icon={<img src={Trex} height={48} alt="A T-Rex as the avatar image" />}
-                iconSize={48}
-                label={'PNG'}
-                ChannelValueProps={{ value: '48', units: 'px' }}
-            />
-        </HeroBanner>
-
-        {/* iconSize 72px */}
-        <HeroBanner>
-            <Hero
-                icon={<VoltageCircledOutline fontSize={'inherit'} />}
-                iconSize={72}
-                label={'SVG'}
-                ChannelValueProps={{ value: '72', units: 'px' }}
-            />
-            <Hero
-                icon={<Schedule fontSize={'inherit'} />}
-                iconSize={72}
-                label={'Mui Icon'}
-                ChannelValueProps={{ value: '72', units: 'px' }}
-            />
-            <Hero
-                icon={<i className="blui-current_circled"></i>}
-                iconSize={72}
-                label={'icon font'}
-                ChannelValueProps={{ value: '72', units: 'px' }}
-            />
-            <Hero
-                icon={<img src={Trex} height={72} alt="A T-Rex as the avatar image" />}
-                iconSize={72}
-                label={'PNG'}
-                ChannelValueProps={{ value: '72', units: 'px' }}
-            />
-        </HeroBanner>
+        {[36, 48, 72].map((iconSize, index) => (
+            <HeroBanner key={index}>
+                <Hero
+                    icon={<VoltageCircledOutline fontSize={'inherit'} />}
+                    iconSize={iconSize}
+                    label={'SVG'}
+                    ChannelValueProps={{ value: iconSize, units: 'px' }}
+                />
+                <Hero
+                    icon={<Schedule fontSize={'inherit'} />}
+                    iconSize={iconSize}
+                    label={'Mui Icon'}
+                    ChannelValueProps={{ value: iconSize, units: 'px' }}
+                />
+                <Hero
+                    icon={<i className="blui-current_circled"></i>}
+                    iconSize={iconSize}
+                    label={'icon font'}
+                    ChannelValueProps={{ value: iconSize, units: 'px' }}
+                />
+                <Hero
+                    icon={<img src={Trex} height={iconSize} alt="A T-Rex as the avatar image" />}
+                    iconSize={iconSize}
+                    label={'PNG'}
+                    ChannelValueProps={{ value: iconSize, units: 'px' }}
+                />
+            </HeroBanner>
+        ))}
     </Box>
 );
