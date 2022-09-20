315d31736559fdd022f5542a7faba931b6bb0fa4
Suraj Karambe <bkarambe@gmail.com>
2022-09-20 17:15:40 +0530

    Fixed snap variant scroll

--- docs/src/componentDocs/AppBar/examples/SnapAppBarExample.tsx
+++ docs/src/componentDocs/AppBar/examples/SnapAppBarExample.tsx
@@ -5,31 +5,32 @@ import { AppBar } from '@brightlayer-ui/react-components';
 import { getBodyFiller } from '../../../shared';
 
 export const SnapAppBarExample = (): JSX.Element => (
-    <Box sx={{ m: '16px 0', backgroundColor: colors.white[600], p: 4 }}>
-        <AppBar
-            variant={'snap'}
-            expandedHeight={200}
-            collapsedHeight={64}
-            scrollThreshold={100}
-            scrollContainerId="appBarSnapContainer"
-            animationDuration={300}
-            sx={{ width: 450, mx: 'auto', zIndex: 'auto' }}
-        >
-            <Toolbar>
-                <Typography variant="h6">Content</Typography>
-            </Toolbar>
-        </AppBar>
-        <Box
-            id="appBarSnapContainer"
-            sx={{
-                height: 140,
-                overflow: 'scroll',
-                backgroundColor: (theme) => theme.palette.background.paper,
-                width: 450,
-                mx: 'auto',
-            }}
-        >
-            {getBodyFiller()}
+    <Box sx={{ m: '16px 0', backgroundColor: colors.white[600], p: 4, overflow: 'hidden' }}>
+        <Box sx={{ mb: 2, overflow: 'hidden', height: 400 }}>
+            <AppBar
+                variant="snap"
+                scrollContainerId={'appbarBodyFiller1'}
+                position={'sticky'}
+                sx={{ width: 450, mx: 'auto', zIndex: 'auto' }}
+            >
+                <Toolbar>
+                    <Typography variant="h6" sx={{ top: 0, position: 'relative' }}>
+                        Content
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Box
+                id="appbarBodyFiller1"
+                sx={{
+                    height: 400,
+                    overflow: 'scroll',
+                    backgroundColor: (theme) => theme.palette.background.paper,
+                    width: 450,
+                    mx: 'auto',
+                }}
+            >
+                {getBodyFiller()}
+            </Box>
         </Box>
     </Box>
 );
