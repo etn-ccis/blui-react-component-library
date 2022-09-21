8d0ed836100dc3a3e08612ef6dea72731030548d
Suraj Karambe <bkarambe@gmail.com>
2022-09-21 15:50:22 +0530

    Added info list item with icon and status color examples

---
+++ docs/src/componentDocs/InfoListItem/examples/InfoListItemWithIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import { CodeBlock, CodeBlockActionButtonRow } from '../../../shared';
+import { InfoListItemWithIconExample } from './InfoListItemWithIconExample';
+
+const codeSnippet = `<InfoListItem title="Info List Item" subtitle="with an icon" icon={<Alarm />} />`;
+
+export const InfoListItemWithIcon = (): JSX.Element => (
+    <Box>
+        <InfoListItemWithIconExample />
+        <CodeBlock code={codeSnippet} language="jsx" />
+        <CodeBlockActionButtonRow
+            copyText={codeSnippet}
+            url="componentDocs/ListItemTag/examples/InfoListItemWithIconExample.tsx"
+        />
+    </Box>
+);
