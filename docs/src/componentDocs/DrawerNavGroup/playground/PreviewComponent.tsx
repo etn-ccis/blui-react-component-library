15b6a5cd9da7ddecb3d76eaa9a4a50c70503fe55
Ekta Ghag <ektasawant99@gmail.com>
2022-09-26 11:33:06 +0530

    update shared props for drawer nav group

--- docs/src/componentDocs/DrawerNavGroup/playground/PreviewComponent.tsx
+++ docs/src/componentDocs/DrawerNavGroup/playground/PreviewComponent.tsx
@@ -8,7 +8,7 @@ import {
     DrawerNavGroup,
     DrawerNavItem,
 } from '@brightlayer-ui/react-components/core/Drawer';
-import { createProps, hideDefaultPropsFromSnippet, removeEmptyLines } from '../../../shared/utilities';
+import { createProps, getIcon, hideDefaultPropsFromSnippet, removeEmptyLines } from '../../../shared/utilities';
 import { PropsType } from '../../../__types__';
 import PreviewComponentWithCode from '../../../shared/PreviewComponentWithCode';
 import LooksOne from '@mui/icons-material/LooksOne';
@@ -19,6 +19,7 @@ export const PreviewComponent = (): JSX.Element => {
     const drawerNavGroupJson = useAppSelector((state: RootState) => state.componentsPropsState.drawerNavGroupComponent);
 
     const drawerNavGroupProps = createProps(drawerNavGroupJson.props as PropsType[]);
+    const drawerNavGroupSharedProps = createProps(drawerNavGroupJson.sharedProps as PropsType[]);
 
     const toggleDefaultProp = (propName: string, currentValue: any): string =>
         hideDefaultPropsFromSnippet(drawerNavGroupJson, propName, currentValue, 'props');
@@ -55,7 +56,7 @@ export const PreviewComponent = (): JSX.Element => {
     return (
         <PreviewComponentWithCode
             previewContent={
-                <Drawer open={true} noLayout sx={{ minHeight: 'auto' }}>
+                <Drawer open={true} activeItem={'Nav Item 1'} noLayout sx={{ minHeight: 'auto' }}>
                     <DrawerHeader icon={<Menu />} title={'Header Title'} />
 
                     <DrawerBody sx={{ flex: '1 1 auto' }} backgroundColor={'transparent'}>
@@ -63,6 +64,22 @@ export const PreviewComponent = (): JSX.Element => {
                             title={drawerNavGroupProps.title}
                             titleColor={drawerNavGroupProps.titleColor}
                             titleDivider={drawerNavGroupProps.titleDivider}
+                            activeItemBackgroundColor={drawerNavGroupSharedProps.activeItemBackgroundColor}
+                            activeItemBackgroundShape={drawerNavGroupSharedProps.activeItemBackgroundShape}
+                            activeItemFontColor={drawerNavGroupSharedProps.activeItemFontColor}
+                            activeItemIconColor={drawerNavGroupSharedProps.activeItemIconColor}
+                            chevron={drawerNavGroupSharedProps.chevron}
+                            chevronColor={drawerNavGroupSharedProps.chevronColor}
+                            collapseIcon={getIcon(drawerNavGroupSharedProps.collapseIcon)}
+                            disableActiveItemParentStyles={drawerNavGroupSharedProps.disableActiveItemParentStyles}
+                            divider={drawerNavGroupSharedProps.divider}
+                            expandIcon={getIcon(drawerNavGroupSharedProps.expandIcon)}
+                            hidePadding={drawerNavGroupSharedProps.hidePadding}
+                            itemFontColor={drawerNavGroupSharedProps.itemFontColor}
+                            itemIconColor={drawerNavGroupSharedProps.itemIconColor}
+                            nestedBackgroundColor={drawerNavGroupSharedProps.nestedBackgroundColor}
+                            nestedDivider={drawerNavGroupSharedProps.nestedDivider}
+                            ripple={drawerNavGroupSharedProps.ripple}
                         >
                             <DrawerNavItem icon={<LooksOne />} itemID={'Nav Item 1'} title={'Nav Item 1'} />
                             <DrawerNavItem icon={<LooksTwo />} itemID={'Nav Item 2'} title={'Nav Item 2'} />
