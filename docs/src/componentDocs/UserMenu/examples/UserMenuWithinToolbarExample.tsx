ccde5ed8254c93d4dc9b0d398fc654fc103229ab
Suraj Karambe <bkarambe@gmail.com>
2022-09-26 17:54:51 +0530

    Added within toolbar example for user menu

---
+++ docs/src/componentDocs/UserMenu/examples/UserMenuWithinToolbarExample.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import { Spacer, UserMenu } from '@brightlayer-ui/react-components';
+import { Email, ExitToApp, Settings } from '@mui/icons-material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export const UserMenuWithinToolbarExample = (): JSX.Element => (
+    <Box sx={{ width: 300 }}>
+        <AppBar position={'relative'} color={'primary'}>
+            <Toolbar sx={{ px: 2, minHeight: 'unset', height: '4rem' }}>
+                <Typography variant={'h6'}>Toolbar Title</Typography>
+                <Spacer flex={1} />
+                <UserMenu
+                    avatar={<Avatar />}
+                    menuGroups={[
+                        {
+                            items: [
+                                {
+                                    title: 'Settings',
+                                    icon: <Settings />,
+                                },
+                                {
+                                    title: 'Contact Us',
+                                    icon: <Email />,
+                                },
+                                {
+                                    title: 'Log Out',
+                                    icon: <ExitToApp />,
+                                    divider: true,
+                                },
+                                {
+                                    itemID: '4',
+                                    title: 'v1.03.54',
+                                    hidePadding: true,
+                                    sx: {
+                                        '& .BluiInfoListItem-root': {
+                                            height: 40,
+                                        },
+                                        '& .BluiInfoListItem-title': {
+                                            fontSize: 12,
+                                        },
+                                    },
+                                },
+                            ],
+                        },
+                    ]}
+                    menuTitle={'Jane Doe'}
+                    menuSubtitle={'Account Manager'}
+                    onOpen={(): void => {}}
+                    onClose={(): void => {}}
+                />
+            </Toolbar>
+        </AppBar>
+        <Box
+            sx={{
+                height: 90,
+                backgroundColor: 'background.paper',
+                p: 2,
+            }}
+        >
+            <Typography variant={'body1'}>Body Content Goes Here</Typography>
+        </Box>
+    </Box>
+);
