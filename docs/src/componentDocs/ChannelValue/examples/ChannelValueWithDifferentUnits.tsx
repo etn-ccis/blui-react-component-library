7dff1bf3e237295f61d5f49b3420742591f4edf2
Suraj Karambe <bkarambe@gmail.com>
2022-09-26 11:42:59 +0530

    Added example for channel value with unit space prop

--- docs/src/componentDocs/ChannelValue/examples/ChannelValueWithDifferentUnits.tsx
+++ docs/src/componentDocs/ChannelValue/examples/ChannelValueWithDifferentUnits.tsx
@@ -5,7 +5,8 @@ import * as colors from '@brightlayer-ui/colors';
 import { ChannelValueWithDifferentUnitsExample } from './ChannelValueWithDifferentUnitsExample';
 
 const codeSnippet = `<Box sx={{ display: 'flex', flexDirection: 'column', '& .BluiChannelValue-root': { my: 0.25 } }}>
-    <ChannelValue value="85" units="kWh" />
+    <ChannelValue value="85" units="kWh" unitSpace="hide" />
+    <ChannelValue value="85" units="kWh" unitSpace="show" />
     <ChannelValue value="97" units="°F" />
     <ChannelValue value="32" units="°C" />
     <ChannelValue value="100" units="%" />
--- docs/src/componentDocs/ChannelValue/examples/ChannelValueWithDifferentUnitsExample.tsx
+++ docs/src/componentDocs/ChannelValue/examples/ChannelValueWithDifferentUnitsExample.tsx
@@ -4,7 +4,8 @@ import { ChannelValue } from '@brightlayer-ui/react-components';
 
 export const ChannelValueWithDifferentUnitsExample = (): JSX.Element => (
     <Box sx={{ display: 'flex', flexDirection: 'column', '& .BluiChannelValue-root': { my: 0.25 } }}>
-        <ChannelValue value="85" units="kWh" />
+        <ChannelValue value="85" units="kWh" unitSpace="hide" />
+        <ChannelValue value="85" units="kWh" unitSpace="show" />
         <ChannelValue value="97" units="°F" />
         <ChannelValue value="32" units="°C" />
         <ChannelValue value="100" units="%" />
