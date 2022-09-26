b46647db97d445ac0a5ffa323dd41062cfbe157b
Suraj Karambe <bkarambe@gmail.com>
2022-09-26 12:51:06 +0530

    Added user menu with header example

---
+++ docs/src/componentDocs/UserMenu/examples/UserMenuWithMenuHeader.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import { CodeBlock, CodeBlockActionButtonRow } from '../../../shared';
+import * as colors from '@brightlayer-ui/colors';
+import { UserMenuWithMenuHeaderExample } from './UserMenuWithMenuHeaderExample';
+
+const codeSnippet = `<UserMenu
+    avatar={<Avatar>MH</Avatar>}
+    menuGroups={[
+        {
+            items: [
+                {
+                    title: 'Settings',
+                    icon: <Settings />,
+                },
+            ],
+        },
+    ]}
+    menuTitle="Sample Title"
+    menuSubtitle="Sample Subtitle"
+/>`;
+
+export const UserMenuWithMenuHeader = (): JSX.Element => (
+    <Box>
+        <Box sx={{ m: '16px 0', backgroundColor: colors.white[600], p: 4, display: 'flex', justifyContent: 'center' }}>
+            <UserMenuWithMenuHeaderExample />
+        </Box>
+        <CodeBlock code={codeSnippet} language="jsx" />
+        <CodeBlockActionButtonRow
+            copyText={codeSnippet}
+            url="componentDocs/UserMenu/examples/UserMenuWithMenuHeaderExample.tsx"
+        />
+    </Box>
+);
