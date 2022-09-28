ec98882e018fa5e6dfbdfed6e1a185b1087bcfa4
Suraj Karambe <bkarambe@gmail.com>
2022-09-28 14:18:04 +0530

    Added basic toolbar menu example

--- docs/src/__configuration__/navigationMenu/navigation.tsx
+++ docs/src/__configuration__/navigationMenu/navigation.tsx
@@ -39,6 +39,7 @@ import DrawerLayoutExamples from '../../componentDocs/DrawerLayout/markdown/Draw
 import AppBarExamples from '../../componentDocs/AppBar/markdown/AppBarExamples.mdx';
 import UserMenuExamples from '../../componentDocs/UserMenu/markdown/UserMenuExamples.mdx';
 import ChannelValueExamples from '../../componentDocs/ChannelValue/markdown/ChannelValueExamples.mdx';
+import ToolbarMenuExamples from '../../componentDocs/ToolbarMenu/markdown/ToolbarMenuExamples.mdx';
 
 // Playground components
 import { AppBarPlaygroundComponent } from '../../componentDocs/AppBar/playground/PlaygroundPage';
@@ -488,7 +489,7 @@ export const pageDefinitions: SimpleGroupNavGroupItem[] = [
                     {
                         title: 'Examples',
                         url: 'examples',
-                        component: <DummyExamplesComponent />,
+                        component: <ToolbarMenuExamples />,
                     },
                     {
                         title: 'API Docs',
