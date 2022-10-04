95b079f7dea469a818662856a20c337a38cb6e12
jeffvg <jeffvgreiner@eaton.com>
2022-10-04 14:02:41 -0400

    update pixel example

--- docs/src/componentDocs/Spacer/examples/SpacerWithPixelExample.tsx
+++ docs/src/componentDocs/Spacer/examples/SpacerWithPixelExample.tsx
@@ -5,14 +5,14 @@ import { Spacer } from '@brightlayer-ui/react-components';
 export const SpacerWithPixelExample = (): JSX.Element => (
     <Box>
         <Box sx={{ width: '300px', height: '56px', display: 'flex' }}>
-            <Spacer maxWidth={75} style={{ background: '#4da3d4' }}>
-                75
+            <Spacer maxWidth={25} style={{ background: '#4da3d4' }}>
+                25
             </Spacer>
             <Spacer maxWidth={75} style={{ background: '#f5db6d' }}>
                 75
             </Spacer>
-            <Spacer maxWidth={150} style={{ background: '#da7777' }}>
-                150
+            <Spacer maxWidth={200} style={{ background: '#da7777' }}>
+                200
             </Spacer>
         </Box>
 
@@ -20,11 +20,11 @@ export const SpacerWithPixelExample = (): JSX.Element => (
             <Spacer height={25} style={{ background: '#4da3d4' }}>
                 25
             </Spacer>
-            <Spacer height={25} style={{ background: '#f5db6d' }}>
-                25
-            </Spacer>
-            <Spacer height={50} style={{ background: '#da7777' }}>
+            <Spacer height={50} style={{ background: '#f5db6d' }}>
                 50
+            </Spacer>
+            <Spacer height={75} style={{ background: '#da7777' }}>
+                75
             </Spacer>
         </Box>
     </Box>
