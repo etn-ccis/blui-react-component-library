353adb3b2fda68ffbd895ff3f3edb3ec600b3441
Suraj Karambe <bkarambe@gmail.com>
2022-10-12 18:30:57 +0530

    Cleaned up drawer anv group example

--- docs/src/componentDocs/DrawerNavGroup/examples/CustomNavGroupTitleContent.tsx
+++ docs/src/componentDocs/DrawerNavGroup/examples/CustomNavGroupTitleContent.tsx
@@ -23,7 +23,7 @@ const codeSnippet = `<Drawer open={true} width={250}>
 export const CustomNavGroupTitleContent = (): JSX.Element => (
     <Box>
         <CustomNavGroupTitleContentExample />
-        <CodeBlock code={codeSnippet} language="jsx" dataLine="3-13" />
+        <CodeBlock code={codeSnippet} language="jsx" dataLine="4-9" />
         <CodeBlockActionButtonRow
             copyText={codeSnippet}
             url="componentDocs/DrawerNavGroup/examples/CustomNavGroupTitleContentExample.tsx"
--- docs/src/componentDocs/DrawerNavGroup/examples/MultipleDrawerNavGroups.tsx
+++ docs/src/componentDocs/DrawerNavGroup/examples/MultipleDrawerNavGroups.tsx
@@ -6,12 +6,12 @@ import { MultipleDrawerNavGroupsExample } from './MultipleDrawerNavGroupsExample
 const codeSnippet = `<Drawer open={true} width={250}>
     <DrawerBody>
         <DrawerNavGroup title="Locations" hidePadding divider>
-            <DrawerNavItem title="Regional" itemID="1" divider={false} />
-            <DrawerNavItem title="National" itemID="2" divider={false} />
+            <DrawerNavItem title="Regional" itemID="1" />
+            <DrawerNavItem title="National" itemID="2" />
         </DrawerNavGroup>
         <DrawerNavGroup title="Status" hidePadding divider>
-            <DrawerNavItem title="Network" itemID="3" divider={false} />
-            <DrawerNavItem title="Node" itemID="4" divider={false} />
+            <DrawerNavItem title="Network" itemID="3" />
+            <DrawerNavItem title="Node" itemID="4" />
         </DrawerNavGroup>
     </DrawerBody>
 </Drawer>
