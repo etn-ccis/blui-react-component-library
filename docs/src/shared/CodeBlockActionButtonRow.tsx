9e932ca97131a51a10b32ffe0a32938ca790ffe6
Suraj Karambe <bkarambe@gmail.com>
2022-10-12 16:14:34 +0530

    Rearranged the copy and github button

--- docs/src/shared/CodeBlockActionButtonRow.tsx
+++ docs/src/shared/CodeBlockActionButtonRow.tsx
@@ -23,6 +23,7 @@ export const CodeBlockActionButtonRow: React.FC<CodeBlockActionButtonRowProps> =
 
     return (
         <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 1, ...sx }}>
+            {url !== '' && <FullCodeOnGithub sx={{ ml: 1 }} url={url} />}
             {copyText !== '' && (
                 <CopyToClipboard
                     title={title}
@@ -33,7 +34,6 @@ export const CodeBlockActionButtonRow: React.FC<CodeBlockActionButtonRowProps> =
                     toolTipProps={props.toolTipProps}
                 />
             )}
-            {url !== '' && <FullCodeOnGithub sx={{ ml: 1 }} url={url} />}
         </Box>
     );
 };
