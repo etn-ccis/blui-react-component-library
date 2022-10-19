bcd1833e6c223db6d561a66e91c10e1f00d3d6c2
Ekta Ghag <ektasawant99@gmail.com>
2022-10-19 19:18:25 +0530

    add error message for required props

--- docs/src/shared/DocTextField.tsx
+++ docs/src/shared/DocTextField.tsx
@@ -8,15 +8,16 @@ type TextFieldProps = MuiTextFieldProps & {
 
 export const DocTextField = (props: TextFieldProps): JSX.Element => {
     const { propData, ...textFieldProps } = props;
-
+    const validInput = propData.required && propData.inputValue === '';
     return (
         <MuiTextField
             {...textFieldProps}
             variant={'filled'}
             value={propData.inputValue}
             label={`${propData.label ? propData.label : propData.propName}: ${propData.propType}`}
-            helperText={propData.helperText}
+            helperText={validInput ? `${propData.propName} is required` : propData.helperText}
             disabled={propData.disabled}
+            error={validInput}
         />
     );
 };
