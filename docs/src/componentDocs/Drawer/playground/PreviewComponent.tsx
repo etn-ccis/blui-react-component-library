25404cf40b1452d87df744781c97471c6ed93371
Ekta Ghag <ektasawant99@gmail.com>
2022-10-21 11:03:13 +0530

    remove commented code

--- docs/src/componentDocs/Drawer/playground/PreviewComponent.tsx
+++ docs/src/componentDocs/Drawer/playground/PreviewComponent.tsx
@@ -89,28 +89,6 @@ export const PreviewComponent = (): JSX.Element => {
 
     const toggleOpenProp = (open: boolean): string => `updateOpenProp(${open ? `false` : `true`})`;
 
-    //     const toggleAppContent = (): string =>
-    //         variantIsRail
-    //             ? `<Box>App Content Here.</Box>`
-    //             : `<Box>
-    //     <AppBar position="static">
-    //         <Toolbar>
-    //             <IconButton
-    //                 size="large"
-    //                 edge="start"
-    //                 color="inherit"
-    //                 aria-label="menu"
-    //                 sx={{ mr: 2 }}
-    //                 onClick={(): void => ${toggleOpenProp(drawerProps.open)}}
-    //             >
-    //                 <Menu />
-    //             </IconButton>
-    //             <Typography variant="h6">Toolbar</Typography>
-    //         </Toolbar>
-    //     </AppBar>
-    //     <Box>App Content Here.</Box>
-    // </Box>`;
-
     const updateHeaderAsPerVariant = (): string => {
         if (variantIsTemporary) {
             return `
