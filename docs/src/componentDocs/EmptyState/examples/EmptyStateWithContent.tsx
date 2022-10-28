82fbc06852ee7406b95fe0f27d9ecee24c8d692a
Suraj Karambe <bkarambe@gmail.com>
2022-10-28 15:45:08 +0530

    Updated empty state description

--- docs/src/componentDocs/EmptyState/examples/EmptyStateWithContent.tsx
+++ docs/src/componentDocs/EmptyState/examples/EmptyStateWithContent.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { CodeBlock, CodeBlockActionButtonRow } from '../../../shared';
-import * as colors from '@brightlayer-ui/colors';
 import { EmptyStateWithContentExample } from './EmptyStateWithContentExample';
 
 const codeSnippet = `<EmptyState
@@ -12,10 +11,8 @@ const codeSnippet = `<EmptyState
 
 export const EmptyStateWithContent = (): JSX.Element => (
     <Box>
-        <Box sx={{ m: '16px 0', backgroundColor: colors.white[600], p: 4 }}>
-            <EmptyStateWithContentExample />
-        </Box>
-        <CodeBlock code={codeSnippet} language="jsx" dataLine={'3-4'} />
+        <EmptyStateWithContentExample />
+        <CodeBlock code={codeSnippet} language="jsx" dataLine="3-4" />
         <CodeBlockActionButtonRow
             copyText={codeSnippet}
             url="componentDocs/EmptyState/examples/EmptyStateWithContentExample.tsx"
