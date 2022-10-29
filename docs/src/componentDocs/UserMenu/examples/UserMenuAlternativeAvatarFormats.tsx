876ec2102d1650dd4a046e250a185c35521512e5
joebochill <boyle.p.joseph@gmail.com>
2022-10-29 11:29:23 -0400

    Update remaining examples

--- docs/src/componentDocs/UserMenu/examples/UserMenuAlternativeAvatarFormats.tsx
+++ docs/src/componentDocs/UserMenu/examples/UserMenuAlternativeAvatarFormats.tsx
@@ -3,43 +3,41 @@ import { Box } from '@mui/material';
 import { CodeBlock, CodeBlockActionButtonRow } from '../../../shared';
 import { UserMenuAlternativeAvatarFormatsExample } from './UserMenuAlternativeAvatarFormatsExample';
 
-const codeSnippet = `<Box sx={{ display: 'flex', justifyContent: 'space-between', width: 150 }}>
-    <UserMenu
-        avatar={<Avatar src='../images/tRex.png' alt={'User Avatar'} />}
-        menuGroups={[
-            {
-                items: [
-                    {
-                        title: 'Settings',
-                        icon: <Settings />,
-                    },
-                ],
-            },
-        ]}
-    />
-    <UserMenu
-        avatar={
-            <Avatar>
-                <Pets />
-            </Avatar>
-        }
-        menuGroups={[
-            {
-                items: [
-                    {
-                        title: 'Settings',
-                        icon: <Settings />,
-                    },
-                ],
-            },
-        ]}
-    />
-</Box>`;
+const codeSnippet = `<UserMenu
+    avatar={<Avatar src='../images/tRex.png' alt={'User Avatar'} />}
+    menuGroups={[
+        {
+            items: [
+                {
+                    title: 'Settings',
+                    icon: <Settings />,
+                },
+            ],
+        },
+    ]}
+/>
+<UserMenu
+    avatar={
+        <Avatar>
+            <Pets />
+        </Avatar>
+    }
+    menuGroups={[
+        {
+            items: [
+                {
+                    title: 'Settings',
+                    icon: <Settings />,
+                },
+            ],
+        },
+    ]}
+/>`;
 
 export const UserMenuAlternativeAvatarFormats = (): JSX.Element => (
     <Box>
         <UserMenuAlternativeAvatarFormatsExample />
-        <CodeBlock code={codeSnippet} language="jsx" dataLine="3, 16-20" />
+        <CodeBlock code={codeSnippet} language="jsx" dataLine="2, 15-19" />
         <CodeBlockActionButtonRow
             copyText={codeSnippet}
             url="componentDocs/UserMenu/examples/UserMenuAlternativeAvatarFormatsExample.tsx"
