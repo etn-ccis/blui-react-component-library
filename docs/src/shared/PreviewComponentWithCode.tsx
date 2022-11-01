014de32b6a875131587b4fcab33a3ff7b2049a0b
joebochill <boyle.p.joseph@gmail.com>
2022-11-01 10:57:56 -0400

    Add comment for calculations

--- docs/src/shared/PreviewComponentWithCode.tsx
+++ docs/src/shared/PreviewComponentWithCode.tsx
@@ -17,6 +17,10 @@ const PreviewComponentWithCode: React.FC<PreviewComponentProps> = (props): JSX.E
             <Box
                 sx={{
                     p: 2,
+                    /**
+                     * We take the height of the appbar/tabs out of the 70vh portion
+                     * 64px (56 on xs) for AppBar + 49px for Tabs = 113/105
+                     * */
                     height: { xs: 'calc(70vh - 105px)', sm: 'calc(70vh - 113px)' },
                     display: 'flex',
                     justifyContent: 'center',
