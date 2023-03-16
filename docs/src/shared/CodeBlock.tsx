881970dabbebaa76b66a2411bb408754a5bfe936
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-03-16 14:44:24 -0400

    update mdx to use codeblock

--- docs/src/shared/CodeBlock.tsx
+++ docs/src/shared/CodeBlock.tsx
@@ -1,37 +1,40 @@
-import React, { HTMLAttributes, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Prism from 'prismjs';
-import Box from '@mui/material/Box';
+import Box, { BoxProps } from '@mui/material/Box';
 import * as Colors from '@brightlayer-ui/colors';
 
-export type CodeBlockProps = HTMLAttributes<HTMLDivElement> & {
+export type CodeBlockProps = BoxProps & {
     code: string;
     language: string;
     dataLine?: string;
 };
 
 export const CodeBlock: React.FC<CodeBlockProps> = (props): JSX.Element => {
-    const { code, language, dataLine, ...divProps } = props;
+    const { code, language, dataLine, sx, ...boxProps } = props;
     useEffect(() => {
         Prism.highlightAll();
     }, [code]);
     return (
         <Box
-            sx={{
-                '.line-highlight::before': {
-                    display: 'none',
+            sx={[
+                {
+                    '.line-highlight::before': {
+                        display: 'none',
+                    },
+                    '.line-highlight::after': { display: 'none', },
+                    height: '100%', display:
+                        'flex',
                 },
-                '.line-highlight::after': {
-                    display: 'none',
-                },
-                height: '100%',
-                display: 'flex',
-            }}
-            {...divProps}
+                ...(Array.isArray(sx) ? sx : [sx]),]
+            }
+            {...boxProps}
             className="Code"
         >
-            <pre
-                data-line={dataLine}
-                style={{ margin: 0, width: '100%', backgroundColor: Colors.black[800], borderRadius: 4 }}
+            <pre data-line={dataLine}
+                style={{
+                    margin: 0, width: '100%', backgroundColor: Colors.black[800],
+                    borderRadius: 4
+                }}
             >
                 <code style={{ fontFamily: 'monospace' }} className={`language-${language}`}>
                     {code}
