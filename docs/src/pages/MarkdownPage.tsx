9f714ee7fea8d29ed4230c28ec99f304187a5b18
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-03-16 15:14:43 -0400

    cleanup

--- docs/src/pages/MarkdownPage.tsx
+++ docs/src/pages/MarkdownPage.tsx
@@ -1,8 +1,5 @@
 import React, { HTMLAttributes } from 'react';
 import { PageContent } from './PageContent';
-// import { useBackgroundColor } from '../hooks/useBackgroundColor';
-// import { useTheme } from '@mui/material';
-// import * as Colors from '@brightlayer-ui/colors';
 
 export type MarkdownPageProps = HTMLAttributes<HTMLDivElement> & {
     title: string;
@@ -15,16 +12,6 @@ export type MarkdownPageProps = HTMLAttributes<HTMLDivElement> & {
 
 export const MarkdownPage: React.FC<MarkdownPageProps> = (props): JSX.Element => {
     const { markdown: Markdown, noPadding, wideLayout, ...divProps } = props;
-    // const theme = useTheme();
-
-    // let backgroundColor = background;
-    // if (background === 'light') {
-    //     backgroundColor = theme.palette.background.paper;
-    // } else if (background === 'dark') {
-    //     backgroundColor = theme.palette.mode === 'light' ? Colors.white[200] : theme.palette.background.default;
-    // }
-    // useBackgroundColor(backgroundColor);
-
     return (
         <div {...divProps}>
             <PageContent noPadding={noPadding} wideLayout={wideLayout}>
@@ -34,6 +21,3 @@ export const MarkdownPage: React.FC<MarkdownPageProps> = (props): JSX.Element =>
     );
 };
 MarkdownPage.displayName = 'MarkdownPage';
-// MarkdownPage.defaultProps = {
-// background: 'light',
-// };
