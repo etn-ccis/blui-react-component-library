1127054eb15dcb23c6f68d09f375d7cecb1c93fb
Suraj Karambe <surajakarambe@eaton.com>
2023-03-23 16:05:15 +0530

    Removed react icon from comp catalog

--- docs/src/pages/ComponentCatalogGrids.tsx
+++ docs/src/pages/ComponentCatalogGrids.tsx
@@ -1,39 +1,7 @@
 import React from 'react';
-import { Grid, Tooltip, Chip, Box } from '@mui/material';
+import { Grid } from '@mui/material';
 import { componentCatalogLinks } from './ComponentCatalogLinks';
-import { ReactBlue } from '../assets/icons';
 import { InfoCard } from '../shared/components/InfoCard';
-
-type CatalogStorybookLinkProp = {
-    content: string;
-};
-
-const CatalogStorybookLink: React.FC<CatalogStorybookLinkProp> = (props) => {
-    const { content } = props;
-    if (content.startsWith('http')) {
-        return (
-            <Chip
-                label="React"
-                avatar={<ReactBlue sx={{ backgroundColor: 'transparent !important' }} />}
-                onClick={(): void => {
-                    window.open(content, '_blank');
-                }}
-            />
-        );
-    }
-    return (
-        <Tooltip title={content} placement={'top'} arrow>
-            <div>
-                <Chip
-                    disabled
-                    label="React"
-                    variant={'outlined'}
-                    avatar={<ReactBlue sx={{ backgroundColor: 'transparent !important', filter: 'grayscale(100%)' }} />}
-                />
-            </div>
-        </Tooltip>
-    );
-};
 
 export const ComponentCatalogGrids: React.FC = () => (
     <Grid container spacing={6} sx={{ mt: 4 }}>
@@ -45,18 +13,9 @@ export const ComponentCatalogGrids: React.FC = () => (
                     aspectRatio={'3x2'}
                     title={link.title}
                     description={''}
-                    descriptionContent={
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                flexWrap: 'wrap',
-                                gap: 1,
-                            }}
-                        >
-                            <CatalogStorybookLink content={link.react} />
-                        </Box>
-                    }
+                    onClick={(): void => {
+                        window.open(link.react, '_blank');
+                    }}
                 />
             </Grid>
         ))}
