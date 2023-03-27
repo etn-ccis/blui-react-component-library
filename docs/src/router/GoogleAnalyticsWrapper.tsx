20dc9ca469ea7bfc982c33dac98f7d2fe2610db3
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-03-27 16:25:57 -0400

    remove location

--- docs/src/router/GoogleAnalyticsWrapper.tsx
+++ docs/src/router/GoogleAnalyticsWrapper.tsx
@@ -1,14 +1,9 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
 export const GoogleAnalyticsWrapper: React.FC = () => {
-    const location = useLocation();
-    useEffect(() => {
-        ReactGA.send({
-            hitType: 'window.location.pathname + window.location.search',
-        });
-    }, [location.pathname, location.search]);
+    ReactGA.send({
+        hitType: 'pageview',
+    });
 
     return null;
 };
