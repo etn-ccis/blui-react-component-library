c718de057a1d0822803b71c92e11cba8cf8f2adb
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-03-27 12:05:32 -0400

    add hit type

--- docs/src/router/GoogleAnalyticsWrapper.tsx
+++ docs/src/router/GoogleAnalyticsWrapper.tsx
@@ -5,7 +5,9 @@ import ReactGA from 'react-ga4';
 export const GoogleAnalyticsWrapper: React.FC = () => {
     const location = useLocation();
     useEffect(() => {
-        ReactGA.send(window.location.pathname + window.location.search);
+        ReactGA.send({
+            hitType: "window.location.pathname + window.location.search"
+        });
     }, [location.pathname, location.search]);
 
     return null;
