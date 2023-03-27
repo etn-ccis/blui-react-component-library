a2c10f428cbac58a90d3db23689bcf8f9197dfa4
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-03-27 16:05:42 -0400

    remove pageview

--- docs/src/router/GoogleAnalyticsWrapper.tsx
+++ docs/src/router/GoogleAnalyticsWrapper.tsx
@@ -6,7 +6,7 @@ export const GoogleAnalyticsWrapper: React.FC = () => {
     const location = useLocation();
     useEffect(() => {
         ReactGA.send({
-            hitType: 'window.location.pathname + window.location.search + pageview',
+            hitType: 'window.location.pathname + window.location.search',
         });
     }, [location.pathname, location.search]);
 
