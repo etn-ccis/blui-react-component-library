b9e2f6e83f580476f99e1d316c7e777a484980f3
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-03-28 15:31:23 -0400

    update master gaID

--- docs/src/router/GoogleAnalyticsWrapper.tsx
+++ docs/src/router/GoogleAnalyticsWrapper.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-// import ReactGA from 'react-ga4';
 
 export const GoogleAnalyticsWrapper: React.FC = () => {
     const location = useLocation();
