3209b6f4c4f7fc0378ce259b0e8cac15d69ddef1
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-03-28 12:50:52 -0400

    set gTag script change ga wrapper

--- docs/src/router/GoogleAnalyticsWrapper.tsx
+++ docs/src/router/GoogleAnalyticsWrapper.tsx
@@ -1,9 +1,15 @@
-import ReactGA from 'react-ga4';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+// import ReactGA from 'react-ga4';
 
 export const GoogleAnalyticsWrapper: React.FC = () => {
-    ReactGA.send({
-        hitType: 'pageview',
-    });
-
-    return null;
+    const location = useLocation();
+        useEffect(() => {
+            window.gtag("event", "page_view", {
+                page_path: location.pathname + location.search + location.hash,
+                page_search: location.search,
+                page_hash: location.hash,
+            });
+        }, [location]);
+        return null;
 };
