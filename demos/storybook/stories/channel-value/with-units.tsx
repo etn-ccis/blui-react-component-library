914a59e18ffd298a1ee6487ad920ad38ecf8421f
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2024-02-02 12:59:41 -0500

    Blui 5253 remove storybook

--- demos/storybook/stories/channel-value/with-units.tsx
+++
@@ -1,8 +0,0 @@
-import { ChannelValue } from '@brightlayer-ui/react-components';
-import { text } from '@storybook/addon-knobs';
-import { StoryFnReactReturnType } from '@storybook/react/dist/client/preview/types';
-import React from 'react';
-
-export const withUnits = (): StoryFnReactReturnType => <ChannelValue value={'123'} units={text('units', 'hz')} />;
-
-withUnits.story = { name: 'with units' };
--- demos/storybook/stories/list-item-tag/with-basic-usage.tsx
+++
@@ -1,9 +0,0 @@
-import { ListItemTag } from '@brightlayer-ui/react-components';
-import { text } from '@storybook/addon-knobs';
-import { StoryFnReactReturnType } from '@storybook/react/dist/client/preview/types';
-import React from 'react';
-import { WITH_MIN_PROPS_STORY_NAME } from '../../src/constants';
-
-export const withBasicUsage = (): StoryFnReactReturnType => <ListItemTag label={text('label', 'active')} />;
-
-withBasicUsage.story = { name: WITH_MIN_PROPS_STORY_NAME };
