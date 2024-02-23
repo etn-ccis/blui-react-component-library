781d41b8acd3e8e19ad885f8fde7d3acca7634cc
JeffGreiner-eaton <119693939+JeffGreiner-eaton@users.noreply.github.com>
2024-02-23 14:54:44 -0500

    Update components/src/core/InfoListItem/InfoListItem.tsx
    Co-authored-by: Joseph Boyle <boyle.p.joseph@gmail.com>

--- components/src/core/InfoListItem/InfoListItem.tsx
+++ components/src/core/InfoListItem/InfoListItem.tsx
@@ -244,7 +244,7 @@ const InfoListItemRender: React.ForwardRefRenderFunction<unknown, InfoListItemPr
 
     const getInfoListItemContent = (): JSX.Element => (
         <>
-            <StatusStripe statusColor={statusColor} className={'statusStripe'} data-testid={'blui-status-stripe'} />
+            <StatusStripe statusColor={statusColor} className={generatedClasses['statusStripe']} data-testid={'blui-status-stripe'} />
             {divider && <InfoListItemDivider divider={divider} className={'divider'} />}
             {(icon || !hidePadding) && getIcon()}
             {leftComponent}
